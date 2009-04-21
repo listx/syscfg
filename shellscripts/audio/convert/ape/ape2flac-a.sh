b523cc719aff739b0e07ca19af84f7eefddb4bda
listdata <linus@ucla.edu>
2009-04-20 23:42:00 -0700

    shell: new APE to FLAC conversion script

---
+++ shellscripts/audio/convert/ape/ape2flac-a.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# convert all *.ape files found into .flac files
+
+# Define error codes
+ARGUMENT_NOT_DIRECTORY=10
+FILE_NOT_FOUND=11
+
+# Check that the argument passed to this script is a directory.
+# If it's not, then exit with an error code.
+if [ ! -d "$1" ]
+then
+	echo "Arg "$1" is NOT a directory!"
+	exit $ARGUMENT_NOT_DIRECTORY
+fi
+
+echo "********************************************************"
+echo $1
+echo ""
+find "$1" -type d -exec /home/listdata/syscfg/shellscripts/audio/convert/ape/ape2flac-b.sh '{}' \;
