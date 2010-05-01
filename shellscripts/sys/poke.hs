c6dccdff29c5e42ea0ecf1faa57451425c370f1b
listdata <linus@ucla.edu>
2010-04-30 18:18:30 -0700

    script: poke: simplified code and output

--- shellscripts/sys/poke.hs
+++ shellscripts/sys/poke.hs
@@ -11,102 +11,10 @@ import IO
 -- for hSetEcho
 import System.IO
 
-keys = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-    "` (backtick)",
-    "~ (tilde)",
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^ (caret)",
-    "&",
-    "*",
-    "(",
-    ")",
-    "_ (underscore)",
-    "- (hyphen/dash/minus)",
-    "+",
-    "=",
-    "[",
-    "]",
-    "{",
-    "}",
-    "\\ (backslash)",
-    "| (pipe)",
-    "; (semicolon)",
-    ": (colon)",
-    "' (apostrophe)",
-    "\" (double quote)",
-    ", (comma)",
-    ". (period)",
-    "<",
-    ">",
-    "/ (forward slash)",
-    "?"
-    ]
+keys =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] ++ -- 26
+        ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] ++ -- 52
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"] ++ -- 62
+        ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", ".", "<", ">", "/", "?"]
 
 puts = putStrLn
 put = putStr
@@ -117,15 +25,16 @@ showrand = do
     r <- getStdRandom $ randomR (0,(length keys) - 1)
     case key of
         'q' -> puts "\npoke: exiting...\n"
-        'j' -> puts $ keys!!((mod r 10) + 52)
-        'k' -> puts $ keys!!((mod r 32) + 62)
-        'l' -> puts $ keys!!(mod r 52)
-        _   -> puts $ keys!!r
+        'j' -> put $ keys!!((mod r 10) + 52)
+        'k' -> put $ keys!!((mod r 32) + 62)
+        'l' -> put $ keys!!(mod r 52)
+        _   -> put $ keys!!r
     if key /= 'q' then showrand else return ()
 
 main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering -- disable buffering from STDIN
+    hSetBuffering stdout NoBuffering -- disable buffering from STDOUT
     hSetEcho stdin False -- disable terminal echo
     puts "\npoke: 'q'     quit"
     puts "      'j'     number"
