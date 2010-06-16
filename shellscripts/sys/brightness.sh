f7d9c72e726c7b781cb74d60fa75c7afe6399446
listdata <linus@ucla.edu>
2010-06-16 11:51:30 -0700

    shellscript/xmonad: backlight toggling support for luxion (Dell Latitutde D505 laptop)

--- shellscripts/sys/brightness.sh
+++ shellscripts/sys/brightness.sh
@@ -1,13 +1,39 @@
-#!/bin/sh
+#!/bin/zsh
+# change backlight settings based on system
+# symlink to /usr/bin/brightness, and call with "sudo brightness"
+# make sure to disable password prompt for it with "sudo visudo"
 
-# since xbacklight always returns a single floating point number, we need to
-# convert it to an integer
+case $HOST in
+    aether)
+        # since xbacklight always returns a single floating point number, we need to
+        # convert it to an integer
+        float=`xbacklight`
+        int=${float/\.*}
+        if [[ $int -eq 0 ]]; then
+            xbacklight -set 100
+        else
+            xbacklight -set 0
+        fi
+    ;;
+    luxion)
+        b=$(cat /sys/class/backlight/dell_backlight/actual_brightness)
+        if [[ $b -lt 7 ]]; then
+            # gradually raise backlight, just like xbacklight
+            let "bi=$b"
+            while [[ bi -lt 7 ]]; do
+                let "bi=$bi+1"
+                echo $bi > /sys/class/backlight/dell_backlight/brightness
+            done
+        else
+            let "bi=$b"
+            while [[ bi -gt 0 ]]; do
+                let "bi=$bi-1"
+                echo $bi > /sys/class/backlight/dell_backlight/brightness
+            done
+        fi
+        ;;
+    *)
+        ;;
+esac
 
-float=`xbacklight`
-int=${float/\.*}
-
-if [ $int -eq 0 ]; then
-    xbacklight -set 100
-else
-    xbacklight -set 0
-fi
+# vim:syntax=zsh
