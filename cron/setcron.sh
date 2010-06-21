947f57220b688461f8a9a7d0b53961254a549994
listdata <linus@ucla.edu>
2010-06-21 00:14:53 -0700

    setcron: cleaner syntax, and also alarm clock

--- cron/setcron.sh
+++ cron/setcron.sh
@@ -8,6 +8,8 @@
 # update the pacman package updates                                            #
 #------------------------------------------------------------------------------#
 
+str=""
+
 # we assign different update times for different systems so that we don't step on each other's toes when updating
 min=""
 case $HOST in
@@ -18,18 +20,17 @@ case $HOST in
     ocean)      min="14" ;;
     *)          min="7" ;;
 esac
-pacman="$min * * * * ping -c 4 -W 10 wikipedia.org && sudo bauerbill -Syy --rebase"
+str+="$min * * * * ping -c 4 -W 10 wikipedia.org && sudo bauerbill -Syy --rebase"
 
 # westminster clock sounds (without hour count)
 # full bell every 3rd hour (hours 0,3,6,9,12,15,18,21)
 # half-bells (different flavors) for morning/midday and evening
-westminster=""
-westminster_a=""
-westminster_b=""
 if [[ $HOST == "exelion" ]]; then
-    westminster="\n0 0,3,6,9,12,15,18,21 * * * ~/syscfg/shellscripts/alarm/westminster-nohour.sh f"
-    westminster_a="\n0 1,2,4,5,7,8,10,11,13,14 * * * ~/syscfg/shellscripts/alarm/westminster-nohour.sh a"
-    westminster_b="\n0 16,17,19,20,22,23 * * * ~/syscfg/shellscripts/alarm/westminster-nohour.sh b"
+    str+="\n0 0,3,6,9,12,15,18,21 * * * ~/syscfg/shellscripts/alarm/westminster-nohour.sh f"
+    str+="\n0 1,2,4,5,7,8,10,11,13,14 * * * ~/syscfg/shellscripts/alarm/westminster-nohour.sh a"
+    str+="\n0 16,17,19,20,22,23 * * * ~/syscfg/shellscripts/alarm/westminster-nohour.sh b"
+    # set alarm clock to go off
+    str+="\n58 4 * * * urxvt -hold -e ~/syscfg/shellscripts/alarm/alarmclock.sh"
 fi
 
-echo "$pacman$westminster$westminster_a$westminster_b" | crontab -
+echo "$str" | crontab -
