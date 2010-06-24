b696da45a7a337df3aa3e092206e76bbd94c1cd8
listdata <linus@ucla.edu>
2010-06-24 11:40:43 -0700

    urxvt: more color scheme tweaks

--- shellscripts/sys/terms/bw.sh
+++ shellscripts/sys/terms/bw.sh
@@ -5,15 +5,15 @@
 export TERM_COLOR_SCHEME_CURRENT='bw'
 
 colors=""
-colors+=" --background #f0f0f0 --color0 #f0f0f0 --color8 #f0f0f0" # background/black regular/black bold
-colors+=" --foreground #404040 --color7 #404040 --color15 #404040" # foreground/text regular/text bold
+colors+=" --background #f0f0f0 --color0 #f0f0f0 --color8 #fafafa" # background/black regular/black bold
+colors+=" --foreground #404040 --color7 #404040 --color15 #202020" # foreground/text regular/text bold
 colors+=" --color1 #a07060 --color9 #d02010" # red (regular, bold)
 colors+=" --color2 #508040 --color10 #509030" # green
 colors+=" --color4 #607080 --color12 #5080b0" # blue
-colors+=" --color3 #a0a070 --color11 #b0b070" # yellow
+colors+=" --color3 #c09030 --color11 #c09030" # yellow
 colors+=" --color6 #70a0a0 --color14 #70b0b0" # cyan
 colors+=" --color5 #605080 --color13 #604090" # magenta
 
-urxvt ${(z)colors} & disown
+urxvt ${(z)colors} -fn "xft:dejavu sans mono:size=10,xft:Kochi Gothic,xft:Baekmuk Gulim" & disown
 
 # vim:syntax=zsh
