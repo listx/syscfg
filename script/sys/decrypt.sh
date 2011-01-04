291bb0ba24cab1d81ca153c005ade41648ebd97a
Linus Arver <linusarver@gmail.com>
2011-01-04 15:53:44 -0800

    script: decrypt: view file with the `less' program

--- script/sys/decrypt.sh
+++ script/sys/decrypt.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 # decrypt files painlessly
 
-gpg2 -o "$(basename "$1" ".gpg")" -d "$1"
+if [[ $1 == "outfile" ]]; then
+    gpg2 -o "$(basename "$2" ".gpg")" -d "$2"
+elif [[ $1 == "viewfile" ]]; then
+    gpg2 -d "$2" | less
+else
+    echo "error: ivalid mode \`$1'"
+fi
 
 # vim:syntax=zsh
