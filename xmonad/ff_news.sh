8fa552e2916ccab7af5b6a640d07ce3d33827053
Linus Arver <linusarver@gmail.com>
2011-01-12 15:35:19 -0800

    xmonad: modularize firefox launching news sites
    A new script, ff_news.sh, handles the news sites-launching
    functionality.

---
+++ xmonad/ff_news.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# launch firefox with a randomized assortment of news sites
+
+# websites to load
+news=(www.lemonde.fr www.zeit.de www.nikkei.com www.hani.co.kr www.sfgate.com)
+hobs=(www.chessbase.com "http://www.reddit.com/r/programming/top?t=week" http://news.ycombinator.com/best www.linuxfr.org)
+
+# randomize sites within each category
+news_r=$(echo ${(F)news} | sort -R | tr '\n' ' ') # now $news_r is a string, with a space between each element
+hobs_r=$(echo ${(F)hobs} | sort -R | tr '\n' ' ')
+sites="$news_r $hobs_r" # combine the strings into one string
+
+firefox ${(s: :)sites} & disown # forcefully split the $sites string into separate words on each space character
+
+# vim:syntax=zsh
