851fd97e12476d7b050d1712a0a6bc64f1ba11f2
Linus Arver <linusarver@gmail.com>
2011-06-23 21:46:26 -0700

    script: cascade.sh, a memory training utility
    Possible uses could be on audio playlists, lines of a play, poetry, etc.

---
+++ script/sys/cascade.sh
@@ -0,0 +1,39 @@
+#!/bin/zsh
+
+# Given a file with the form
+#    1
+#    2
+#    3
+# this script premutes it into
+#    3
+#    2
+#    3
+#    1
+#    2
+#    3
+
+input="$1"
+output="$2"
+
+if [[ -z $input || ! -f $input ]]; then
+    echo "input file cannot be blank"
+    exit 1
+fi
+
+if [[ -z $output ]]; then
+    echo "output file must be specified"
+    exit 1
+fi
+
+<$input while read line; do
+    line_ary+=($line)
+done
+
+:>$output
+num=-1
+for line in $line_ary; do
+    echo "${(F)line_ary[$num,-1]}" >> $output
+    let num-=1
+done
+
+# vim:syntax=zsh
