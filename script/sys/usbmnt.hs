a7ac559a17c92aa3f167c1c2f8ead5ccbc3e1a9e
Linus Arver <linusarver@gmail.com>
2011-07-24 20:31:50 -0700

    usbmnt: new --discover and --no-color options
    Also, various fixes and wording changes.

--- script/sys/usbmnt.hs
+++ script/sys/usbmnt.hs
@@ -18,14 +18,23 @@ data Opts = Opts
     { all_devices :: Bool
     , unmount :: Bool
     , unmount_all :: Bool
+    , discover :: Bool
+    , no_color :: Bool
     } deriving (Data, Typeable, Show, Eq)
 
 progOpts :: Opts
 progOpts = Opts
-    { all_devices = def &= help "mount all mountable USB devices; default FALSE"
+    { all_devices = def &= help "mount all USB devices"
     , unmount = def &= help "choose a USB device to unmount"
     , unmount_all = def &= name "U" &= help "unmount all USB devices"
+    , discover = def &= help "list all mounted/unmounted USB devices"
+    , no_color = def &= help "disable colors"
     }
+    &= details
+        [ "Notes:"
+        , ""
+        , "The default behavior without any options is to try to mount a USB device. Here, `device' means a device under the /dev directory, and in our context, is actually a file system partition. Many USB drives have only a single partition, in which case the term `device' means both the USB drive and the single partition it has."
+        ]
 
 getOpts :: IO Opts
 getOpts = cmdArgs $ progOpts
@@ -39,7 +48,7 @@ _PROGRAM_NAME, _PROGRAM_VERSION, _PROGRAM_INFO, _PROGRAM_DESC, _COPYRIGHT :: Str
 _PROGRAM_NAME = "usbmnt"
 _PROGRAM_VERSION = "0.0.1"
 _PROGRAM_INFO = _PROGRAM_NAME ++ " version " ++ _PROGRAM_VERSION
-_PROGRAM_DESC = "mounts a USB flash drive"
+_PROGRAM_DESC = "mount/unmount USB device(s)"
 _COPYRIGHT = "(C) Linus Arver 2011"
 
 data BlockDevice = BlockDevice
@@ -79,6 +88,21 @@ _BLOCKDEVICE_DEFAULT = BlockDevice
 _ALPHANUM :: String
 _ALPHANUM = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
 
+data Color  = Red
+            | Green
+            | Yellow
+            | Blue
+            | CNone
+    deriving (Show, Eq)
+
+colorize :: Color -> String -> String
+colorize c s = case c of
+    Blue -> "\x1b[1;34m" ++ s ++ "\x1b[0m"
+    Green -> "\x1b[1;32m" ++ s ++ "\x1b[0m"
+    Red -> "\x1b[1;31m" ++ s ++ "\x1b[0m"
+    Yellow -> "\x1b[1;33m" ++ s ++ "\x1b[0m"
+    _ -> s
+
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
@@ -87,7 +111,10 @@ main = do
     user <- getEnv "USER"
     errNo <- argsCheck opts user
     when (errNo > 0) $ exitWith $ ExitFailure errNo
-    prog opts user
+    (devs, takenPaths) <- getDevices opts
+    let mountablePaths = filter (\p -> not $ elem p takenPaths) $ map (\p -> "/mnt/u" ++ show p) [(0::Int)..]
+        devsKV = zip (map show [(1::Int)..]) . zip devs $ mountablePaths
+    prog opts user devsKV
 
 argsCheck :: Opts -> String -> IO Int
 argsCheck Opts{..} user
@@ -97,25 +124,39 @@ argsCheck Opts{..} user
         e :: String -> Int -> IO Int
         e str num = errMsg str >> return num
 
-prog :: Opts -> String -> IO ()
-prog opts@Opts{..} user = do
-    (devs, takenPaths) <- getDevices opts
-    let mountablePaths = filter (\p -> not $ elem p takenPaths) $ map (\p -> "/mnt/u" ++ show p) [(0::Int)..]
-        devsKV = zip (map show [(1::Int)..]) . zip devs $ mountablePaths
-    putStrLn (if (unmount || unmount_all)
-        then "device(s) to unmount:"
-        else "mounted device(s):")
-    mapM_ (\(n, (d, _)) -> putStrLn $ "    " ++ n ++ ") " ++ show d) devsKV
-    putStrLn ""
-    mountMenu opts user devsKV
+prog :: Opts -> String -> [(String, (BlockDevice, FilePath))] -> IO ()
+prog opts@Opts{..} user devsKV
+    | discover = do
+        putStrLn "all devices:"
+        mapM_ (\(_, (d, _)) -> putStrLn $ cshow d) devsKV
+    | otherwise = do
+        putStrLn (if (unmount || unmount_all)
+            then "USB device(s) to unmount:"
+            else "USB device(s) to mount:")
+        mapM_ (\(n, (d, _)) -> putStrLn $ "    " ++ n ++ ") " ++ show' d) devsKV
+        putStrLn ""
+        mountMenu opts user devsKV
+    where
+        cshow :: BlockDevice -> String
+        cshow b@BlockDevice{..}
+            | no_color = show b
+            | otherwise = case mountPoint of
+                Unmounted -> colorize Green $ show b
+                MPath _ -> if not $ null $ getUSBMountPath b
+                    then colorize Blue $ show b
+                    else show b
+                _ -> show b
+        show' :: BlockDevice -> String
+        show' = if not (unmount || unmount_all)
+            then show
+            else unwords . init . words . show
 
 mountMenu :: Opts -> String -> [(String, (BlockDevice, FilePath))] -> IO ()
 mountMenu Opts{..} user devsKV
     | unmount = if length devsKV == 1
         then do putStrLn "only 1 USB device to unmount"
                 tryMount False user (snd . head $ devsKV) >>= exitWith
-        else do putStrLn "choose USB device to unmount (q to exit)"
-                chooseDev user devsKV (tryMount False)
+        else chooseDev prompt user devsKV (tryMount False)
     | unmount_all = do
         putStrLn "unmounting all USB devices..."
         mapM_ (tryMount False user) (map snd devsKV)
@@ -127,31 +168,35 @@ mountMenu Opts{..} user devsKV
     | length devsKV == 1 = do
         putStrLn "only 1 USB device to mount"
         tryMount True user (snd . head $ devsKV) >>= exitWith
-    | otherwise = do
-        putStrLn "choose USB device to mount (q to exit)"
-        chooseDev user devsKV (tryMount True)
+    | otherwise = chooseDev prompt  user devsKV (tryMount True)
+    where
+        prompt :: String
+        prompt = if (unmount || unmount_all)
+            then "choose USB device to unmount (q to exit)"
+            else "choose USB device to mount (q to exit)"
 
-chooseDev :: String -> [(String, (BlockDevice, FilePath))] -> (String -> (BlockDevice, FilePath) -> IO ExitCode) -> IO ()
-chooseDev user devsKV func = do
+chooseDev :: String -> String -> [(String, (BlockDevice, FilePath))] -> (String -> (BlockDevice, FilePath) -> IO ExitCode) -> IO ()
+chooseDev prompt user devsKV func = do
+    putStrLn prompt
     key <- getLine
     case lookup key devsKV of
         Just dev -> func user dev >>= exitWith
         _ -> case key of
             "q" -> return ()
-            _ -> putStrLn "invalid input; try again" >> chooseDev user devsKV func
+            _ -> chooseDev prompt user devsKV func
 
 tryMount :: Bool -> String -> (BlockDevice, FilePath) -> IO ExitCode
-tryMount mount usr (BlockDevice{..}, mp) = do
-    when (null $ mountArgs fsys usr) $ do
-        errMsg $ "invalid file system " ++ squote fsys
+tryMount mount user (BlockDevice{..}, mp) = do
+    when (null $ mountArgs fsys user) $ do
+        errMsg $ "unsupported file system " ++ squote fsys ++ "\nsupported file systems: " ++ (unwords $ map fst (_FILE_SYSTEM_ARGS user))
         exitWith (ExitFailure 1)
     putStr $ (if mount == False then "un" else "")
-        ++ "mounting USB device "
+        ++ "mounting "
         ++ shortname
         ++ " (" ++ fsys ++ ") "
         ++ (if mount == False then "from " ++ show mountPoint else "to " ++ mp)
         ++ ".."
-    (_, _, _, p) <- createProcess $ cmd (mountArgs fsys usr) shortname
+    (_, _, _, p) <- createProcess $ cmd (mountArgs fsys user) shortname
     exitStatus <- waitForProcess p
     if (exitStatus == ExitSuccess)
         then do putStrLn "OK"
@@ -172,10 +217,15 @@ tryMount mount usr (BlockDevice{..}, mp) = do
             , close_fds = False
             }
 
+_FILE_SYSTEM_ARGS :: String -> [(String, String)]
+_FILE_SYSTEM_ARGS user =
+    [ ("ext2", "ext2 -o rw,relatime")
+    , ("vfat", "vfat -o rw,uid=" ++ user ++ ",gid=" ++ user)
+    ]
+
 mountArgs :: String -> String -> String
-mountArgs fsys user = case fsys of
-    "ext2" -> "ext2 -o rw,relatime"
-    "vfat" -> "vfat -o rw,uid=" ++ user ++ ",gid=" ++ user
+mountArgs fsys user = case lookup fsys (_FILE_SYSTEM_ARGS user) of
+    Just a -> a
     _ -> []
 
 getDevices :: Opts -> IO ([BlockDevice], [String])
@@ -190,22 +240,19 @@ getDevices Opts{..} = do
     let toMount = filter (\BlockDevice{..} -> mountPoint == Unmounted) devs''
         toUnmount = filter (\dev -> not $ null $ getUSBMountPath dev) devs''
         takenPaths = filter (not . null) . map getUSBMountPath $ devs''
-    when (null toMount && (not (unmount || unmount_all))) $ do
-        errMsg $ "cannot find any mountable devices"
+    when (not discover && null toMount && (not (unmount || unmount_all))) $ do
+        errMsg $ "cannot find USB devices to mount"
         exitWith (ExitFailure 1)
-    when (null toUnmount && (unmount || unmount_all)) $ do
-        errMsg $ "cannot find any devices to unmount"
+    when (not discover && null toUnmount && (unmount || unmount_all)) $ do
+        errMsg $ "cannot find USB devices to unmount"
         exitWith (ExitFailure 1)
-    if ((unmount || unmount_all) == True)
-        then return (toUnmount, takenPaths)
-        else return (toMount, takenPaths)
+    return $ formatDevs devs'' toMount toUnmount takenPaths
     where
-        getUSBMountPath :: BlockDevice -> String
-        getUSBMountPath BlockDevice{..} = case mountPoint of
-            MPath str -> if take 6 str == "/mnt/u" && (elem (last str) ['0'..'9'])
-                then str
-                else ""
-            _ -> ""
+        formatDevs :: [BlockDevice] -> [BlockDevice] -> [BlockDevice] -> [String] -> ([BlockDevice], [String])
+        formatDevs ds m um takenPaths
+            | discover = (ds, takenPaths)
+            | unmount || unmount_all = (um, takenPaths)
+            | otherwise = (m, takenPaths)
         cmdBlkid = CreateProcess
             { cmdspec = ShellCommand ("sudo blkid -o list")
             , cwd = Nothing
@@ -215,6 +262,13 @@ getDevices Opts{..} = do
             , std_err = Inherit
             , close_fds = False
             }
+
+getUSBMountPath :: BlockDevice -> String
+getUSBMountPath BlockDevice{..} = case mountPoint of
+    MPath str -> if take 6 str == "/mnt/u" && (all (\c -> elem c ['0'..'9']) (drop 6 str))
+        then str
+        else ""
+    _ -> ""
 
 errMsg :: String -> IO ()
 errMsg msg = hPutStrLn stderr $ "error: " ++ msg
