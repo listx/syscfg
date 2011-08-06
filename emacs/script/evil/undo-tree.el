84a505c1ac2c0c0f0febc4b2295505a7b13f8937
Linus Arver <linusarver@gmail.com>
2011-08-05 18:12:05 -0700

    emacs: use Evil (Vimpulse has been deprecated for some time now)
    All of the old Vimpulse mappings have been converted to Evil
    equivalents.
    
    Also, misc cleanup and improved comments.
    
    And since Evil comes with a copy of undo-tree.el, its old location under
    script/ has been removed.

--- emacs/script/evil/undo-tree.el
+++ emacs/script/evil/undo-tree.el
@@ -2,7 +2,7 @@
 ;;; undo-tree.el --- Treat undo history as a tree
 
 
-;; Copyright (C) 2009-2010 Toby Cubitt
+;; Copyright (C) 2009-2011 Toby Cubitt
 
 ;; Author: Toby Cubitt <toby-undo-tree@dr-qubit.org>
 ;; Version: 0.3
@@ -12,16 +12,16 @@
 
 ;; This file is NOT part of Emacs.
 ;;
-;; GNU Emacs is free software: you can redistribute it and/or modify it under
+;; This file is free software: you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free
 ;; Software Foundation, either version 3 of the License, or (at your option)
 ;; any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 ;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 ;; more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License along
 ;; with GNU Emacs. If not, see <http://www.gnu.org/licenses/>.
 
