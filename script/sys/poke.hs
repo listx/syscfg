58542da2ff27dbe28c829458b34c55091fc04820
Linus Arver <linusarver@gmail.com>
2011-08-06 18:08:34 -0700

    poke: new 'h' key option

--- script/sys/poke.hs
+++ script/sys/poke.hs
@@ -4,6 +4,8 @@ module Main where
 import IO
 import System.Random    -- for random numbers
 import System.IO        -- for hSetEcho
+import System
+import Data.List (nub)
 
 keysChar, keysNum, keysPunc, keysCharNum, keysAll :: String
 keysChar = ['a'..'z'] ++ ['A'..'Z']
@@ -12,24 +14,25 @@ keysPunc = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "
 keysCharNum = keysChar ++ keysNum
 keysAll = keysChar ++ keysNum ++ keysPunc
 
-giveKey :: Char -> Int -> Char
-giveKey c n = case c of
+giveKey ::  String -> Char -> Int -> Char
+giveKey keysCustom c n = case c of
     'j'  -> extractChar keysNum
     'k'  -> extractChar keysChar
     'l'  -> extractChar keysCharNum
     ';'  -> extractChar keysPunc
+    'h'  -> extractChar (keysCharNum ++ keysCustom)
     '\n' -> '\n'
     _    -> extractChar keysAll
     where
         extractChar xs = xs!!mod n (length xs)
 
-showRandomKey :: IO ()
-showRandomKey = getChar >>= handleKey
+showRandomKey :: String -> IO ()
+showRandomKey keysCustom = getChar >>= handleKey
     where
         handleKey key = if key /= 'q'
-            then getStdRandom (randomR (0, length keysAll - 1)) >>=
-                 putChar . giveKey key >>
-                 showRandomKey
+            then getStdRandom (randomR (0, length (keysAll ++ keysCustom) - 1)) >>=
+                 putChar . giveKey keysCustom key >>
+                 showRandomKey keysCustom
             else putStrLn "\nBye!" >> return ()
 
 main :: IO ()
@@ -37,11 +40,18 @@ main = do
     hSetBuffering stdin NoBuffering -- disable buffering from STDIN
     hSetBuffering stdout NoBuffering -- disable buffering from STDOUT
     hSetEcho stdin False -- disable terminal echo
-    putStrLn "\npoke: 'q'     quit"
-    putStrLn "      'j'     number"
-    putStrLn "      'k'     letter"
-    putStrLn "      'l'     alphanumeric"
-    putStrLn "      ';'     punctuation"
-    putStrLn "      'ENTER' newline"
-    putStrLn "      else    any\n"
-    showRandomKey -- enter loop
+    as <- getArgs
+    let as' = filter (\c -> elem c keysAll) . nub $ unwords as
+    putStrLn ""
+    mapM_ putStrLn
+        [ "poke: 'q'     quit"
+        , "      'j'     number"
+        , "      'k'     letter"
+        , "      'l'     alphanumeric"
+        , "      ';'     punctuation"
+        , "      'h'     alphanumeric" ++ (if not (null as') then " + " ++ as' else "")
+        , "      'ENTER' newline"
+        , "      else    any"
+        ]
+    putStrLn ""
+    showRandomKey as' -- enter loop
