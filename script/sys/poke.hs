a5bb3f997d9a46a3799fe936f0b0e645e53a17f6
Linus Arver <linusarver@gmail.com>
2011-12-22 19:36:44 -0800

    poke: stylistic changes

--- script/sys/poke.hs
+++ script/sys/poke.hs
@@ -6,8 +6,8 @@ import Data.Binary (decode)
 import qualified Data.ByteString.Lazy as B
 import Data.List (nub)
 import IO
-import System
-import System.IO -- for hSetEcho
+import System (getArgs)
+import System.IO (hSetEcho)
 
 keysChar, keysNum, keysPunc, keysCharNum, keysAll, keysHex :: String
 keysChar = ['a'..'z'] ++ ['A'..'Z']
@@ -18,31 +18,31 @@ keysCharNum = keysChar ++ keysNum
 keysAll = keysChar ++ keysNum ++ keysPunc
 
 giveKey ::  String -> Char -> Int -> Char
-giveKey keysCustom c n = case c of
-    'i'  -> extractChar (keysNum ++ keysHex)
-    'j'  -> extractChar keysNum
-    'k'  -> extractChar keysChar
-    'l'  -> extractChar keysCharNum
-    ';'  -> extractChar keysPunc
-    'h'  -> extractChar (keysCharNum ++ keysCustom)
-    '\n' -> '\n'
-    _    -> extractChar keysAll
+giveKey keysCustom c n = extractChar $ case c of
+    'i'  -> (keysNum ++ keysHex)
+    'j'  -> keysNum
+    'k'  -> keysChar
+    'l'  -> keysCharNum
+    ';'  -> keysPunc
+    'h'  -> (keysCharNum ++ keysCustom)
+    '\n' -> ['\n']
+    _    -> keysAll
     where
-        extractChar xs = xs!!mod n (length xs)
+    extractChar xs = xs!!mod n (length xs)
 
 showRandomKey :: String -> StateT AESRNG IO ()
 showRandomKey keysCustom = handleKey =<< liftIO getChar
     where
-        handleKey key = case key of
-            '\n' -> liftIO (putChar '\n') >> showRandomKey keysCustom
-            'q' -> (liftIO $ putStrLn "\nBye!") >> return ()
-            _ -> mapM_ f [0..(49)::Int] >> (liftIO $ putStrLn []) >> showRandomKey keysCustom
-            where
-                f _ = liftIO
-                    . putChar
-                    . giveKey keysCustom key
-                    . (\n -> mod n (length (keysAll ++ keysCustom) - 1))
-                    =<< aesRandomInt
+    handleKey key = case key of
+        '\n' -> liftIO (putChar '\n') >> showRandomKey keysCustom
+        'q' -> (liftIO $ putStrLn "\nBye!") >> return ()
+        _ -> mapM_ f [0..(49)::Int] >> (liftIO $ putStrLn []) >> showRandomKey keysCustom
+        where
+        f _ = liftIO
+            . putChar
+            . giveKey keysCustom key
+            . (\n -> mod n (length (keysAll ++ keysCustom) - 1))
+            =<< aesRandomInt
 
 aesRandomInt :: StateT AESRNG IO Int
 aesRandomInt = do
@@ -58,19 +58,19 @@ main = do
     hSetEcho stdin False -- disable terminal echo
     as <- getArgs
     let as' = filter (\c -> elem c keysAll) . nub $ unwords as
-    putStrLn ""
     mapM_ putStrLn
-        [ "poke: 'q'     quit"
+        [ []
+        , "poke: 'q'     quit"
         , "      'j'     number"
         , "      'k'     letter"
         , "      'l'     alphanumeric"
         , "      ';'     punctuation"
-        , "      'h'     alphanumeric" ++ (if not (null as') then " + " ++ as' else "")
+        , "      'h'     alphanumeric" ++ (if null as' then [] else " + " ++ as')
         , "      'i'     hexadecimal"
         , "      'ENTER' newline"
         , "      else    any"
+        , []
         ]
-    putStrLn ""
     aesState <- makeSystem -- gather entropy from the system to use as the initial seed
     _ <- runStateT (showRandomKey as') aesState -- enter loop
     return ()
