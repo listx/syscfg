420465c62398aaa64d852571f4aa3812af521d15
Linus Arver <linusarver@gmail.com>
2012-07-26 18:14:16 -0700

    emacs: simplify *scratch* buffer behavior

--- emacs/cfg.el
+++ emacs/cfg.el
@@ -57,39 +57,14 @@ otherwise, close current tab (elscreen)."
 (defun vimlike-:tabe ()
   "Vimlike ':tabe' behavior for creating a new tab and buffer."
   (interactive)
-  (let ((buffer (generate-new-buffer "[No Name]")))
-      ; create new tab
-      (elscreen-create)
-      ; set window's buffer to the newly-created buffer
-      (set-window-buffer (selected-window) buffer)
-      ; set state to normal state
-      (with-current-buffer buffer
-        (evil-normal-state))
-    )
-  )
-;; Adopted from mortenee's answer at http://stackoverflow.com/questions/2662655/automatically-closing-the-scratch-buffer
-(defun my-close-scratch ()
-  ; kill the annoying *scratch* lisp evaluation buffer
-  (kill-buffer "*scratch*")
-  ; create [No Name] buffer if we open emacs without specifying any files to edit
-  (if (not (delq nil (mapcar 'buffer-file-name (buffer-list))))
-      (new-untitled-buffer)
-    ))
-; custom startup hook
-(defun my-emacs-startup-hook ()
-  (my-close-scratch))
-; add custom startup hook to emacs' startup hook
-(add-hook 'emacs-startup-hook 'my-emacs-startup-hook)
-; create a new empty buffer
-(defun new-untitled-buffer ()
-  "Opens a new empty buffer."
-  (interactive)
-  (let ((buf (generate-new-buffer "[No Name]")))
-    (switch-to-buffer buf)
-    (normal-mode)
-    (setq buffer-offer-save t))
+  ; create new tab
+  (elscreen-create)
   )
 ;}}}
+; start *scratch* buffer without the annoying ad
+(setq initial-scratch-message "")
+; set *scratch* mode to fundamental mode (the least specialized mode)
+(setq initial-major-mode 'text-mode)
 
 ; General indentation behavior {{{
 ; pressing TAB inserts a TAB
