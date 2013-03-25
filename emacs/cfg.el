e0da317cb6c8b29fd00b6ff2c0169525d31510fd
Linus Arver <linusarver@gmail.com>
2013-03-24 23:55:17 -0700

    emacs: disable 'buffer list' buffer when opening multiple files

--- emacs/cfg.el
+++ emacs/cfg.el
@@ -433,6 +433,7 @@ otherwise, close current tab (elscreen)."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(inhibit-startup-buffer-menu t)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(scroll-bar-mode nil))
