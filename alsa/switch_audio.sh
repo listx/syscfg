cf3ebdac5fd9740ad4996d8ee01e6af8c59e4eff
Linus Arver <linusarver@gmail.com>
2014-04-18 22:51:28 -0700

    switch_audio.sh: add support for k3

--- alsa/switch_audio.sh
+++ alsa/switch_audio.sh
@@ -4,7 +4,7 @@ if [[ -e /proc/asound/card1 ]]; then
     ln -sf ~/syscfg/alsa/cfg-total-bithead ~/.asoundrc
 else
     case $HOST in
-    k0|k1|k2) ln -sf ~/syscfg/alsa/cfg-$HOST ~/.asoundrc ;;
+    k0|k1|k2|k3) ln -sf ~/syscfg/alsa/cfg-$HOST ~/.asoundrc ;;
     *) echo "Unknown host \`$HOST'" ;;
     esac
 fi
