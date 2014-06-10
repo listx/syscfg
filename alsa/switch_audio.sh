7c2f70a79dd62225531c38e5982a5561fabee749
Linus Arver <linusarver@gmail.com>
2014-06-10 11:22:11 -0700

    switch_audio: swap audio settings manually
    Instead of wishfully asking if there is a sound card attached (and
    assuming that this "extra" sound card is the Total Bithead), we manually
    swap between the Bithead and integrated audio solutions by checking
    which config is currently symlinked by ~/.asoundrc.

--- alsa/switch_audio.sh
+++ alsa/switch_audio.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
-if [[ -e /proc/asound/card1 ]]; then
-    ln -sf ~/syscfg/alsa/cfg-total-bithead ~/.asoundrc
-else
+link="$HOME/.asoundrc"
+linkDest=$(basename $(readlink -f "$link"))
+
+if [[ $linkDest == cfg-total-bithead ]]; then
     case $HOST in
-    k0|k1|k2) ln -sf ~/syscfg/alsa/cfg-$HOST ~/.asoundrc ;;
+    k0|k1|k2) ln -sfv ~/syscfg/alsa/cfg-$HOST $link ;;
     *) echo "Unknown host \`$HOST'" ;;
     esac
+else
+    ln -sfv ~/syscfg/alsa/cfg-total-bithead $link
 fi
