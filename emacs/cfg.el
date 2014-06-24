b7f3e4972903ec208ddc5457383d9778e1a34a41
Linus Arver <linusarver@gmail.com>
2014-06-23 22:13:35 -0700

    emacs: font: Terminus: use less inter-line vertical width
    The font size is still the same.

--- emacs/cfg.el
+++ emacs/cfg.el
@@ -639,7 +639,7 @@ otherwise, close current tab (elscreen)."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "xos4" :family "Terminus")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "xos4" :family "Terminus")))))
 
 ; Backups
 ; put all auto-saves/backups to the temp directory
