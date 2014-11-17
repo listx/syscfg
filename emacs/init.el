936a5290301d76dea5f6342f4586b37d0800a055
Linus Arver <linusarver@gmail.com>
2014-11-16 22:03:57 -0800

    emacs: keymaps: org-mode: reorganize

--- emacs/init.el
+++ emacs/init.el
@@ -7,11 +7,17 @@
 ; remove menu
 (menu-bar-mode -1)
 
+(require 'evil)
+(evil-mode 1)
+
 ; add load path for custom scripts
 (add-to-list 'load-path "~/.emacs.d/script")
 
 ; load per-project indentation style settings
 (load "~/.emacs.d/kakapo-project-settings")
+
+; load keymaps
+(load "~/.emacs.d/maps")
 
 ; MELPA
 (require 'package)
@@ -23,8 +29,6 @@
 ; fix "<dead-grave> is undefined" error
 (require 'iso-transl)
 (require 'cl)
-(require 'evil)
-(evil-mode 1)
 
 ; darken inactive windows
 (require 'hiwin)
@@ -446,74 +450,6 @@ keybinding as it conflicts with Anthy input."
 	(insert "\n- ")
 	(org-insert-item))
 )
-(evil-define-key 'normal org-mode-map (kbd "M-o")
-	(lambda ()
-		(interactive)
-		(end-of-line)
-		(org-insert-heading)
-		(evil-append nil)
-	)
-)
-(evil-define-key 'normal org-mode-map "O"
-	(lambda ()
-		(interactive)
-		(end-of-line)
-		(org-insert-heading)
-		(org-metaright)
-		(evil-append nil)
-	)
-)
-(evil-define-key 'normal org-mode-map "o"
-	(lambda ()
-		(interactive)
-		(end-of-line)
-		(always-insert-item)
-		(evil-append nil)
-	)
-)
-
-(evil-define-key 'normal org-mode-map "t"
-	(lambda ()
-		(interactive)
-		(end-of-line)
-		(org-insert-todo-heading nil)
-		(evil-append nil)
-	)
-)
-(evil-define-key 'normal org-mode-map (kbd "M-t")
-	(lambda ()
-		(interactive)
-		(end-of-line)
-		(org-insert-todo-heading nil)
-		(org-metaright)
-		(evil-append nil)
-	)
-)
-(evil-define-key 'normal org-mode-map (kbd "C-o") 'org-toggle-heading) ; convert a plain list into a heading
-(evil-define-key 'normal org-mode-map "T" 'org-todo) ; mark a TODO item as DONE
-(evil-define-key 'normal org-mode-map ";a" 'org-agenda) ; access agenda buffer
-(evil-define-key 'normal org-mode-map "-" 'org-cycle-list-bullet) ; change bullet style
-
-; allow us to access org-mode keys directly from Evil's Normal mode
-; change item type
-(evil-define-key 'normal org-mode-map (kbd "M-i") 'org-shiftright)
-(evil-define-key 'normal org-mode-map (kbd "M-I") 'org-shiftleft)
-; navigate on a per-item basis
-(evil-define-key 'normal org-mode-map (kbd "M-p") 'org-shiftup)
-(evil-define-key 'normal org-mode-map (kbd "M-n") 'org-shiftdown)
-; heading-based navigation
-(evil-define-key 'normal org-mode-map (kbd "M-l")
-	'org-forward-heading-same-level)
-(evil-define-key 'normal org-mode-map (kbd "M-h")
-	'org-backward-heading-same-level)
-(evil-define-key 'normal org-mode-map (kbd "M-k")
-	'outline-previous-visible-heading)
-(evil-define-key 'normal org-mode-map (kbd "M-j") 'outline-next-visible-heading)
-; move items around, including child nodes
-(evil-define-key 'normal org-mode-map (kbd "M-L") 'org-shiftmetaright)
-(evil-define-key 'normal org-mode-map (kbd "M-H") 'org-shiftmetaleft)
-(evil-define-key 'normal org-mode-map (kbd "M-K") 'org-shiftmetaup)
-(evil-define-key 'normal org-mode-map (kbd "M-J") 'org-shiftmetadown)
 (add-hook 'org-mode-hook
 	'(lambda ()
 		; make TAB go to the other window, and map existing TAB
@@ -522,10 +458,6 @@ keybinding as it conflicts with Anthy input."
 		(define-key org-mode-map [(control tab)] nil)
 	)
 )
-(evil-define-key 'normal org-mode-map (kbd "TAB") 'other-window)
-(evil-define-key 'normal org-mode-map [(control tab)] 'org-cycle)
-
-(evil-define-key 'normal org-mode-map (kbd "<f12>") 'org-html-export-to-html)
 
 ; C
 (add-hook 'c-mode-hook
