d78e12863c642d66bc6be1b02a21adaeafc6ab60
Linus Arver <linusarver@gmail.com>
2014-11-22 18:20:50 -0800

    emacs: move keymaps

--- emacs/maps.el
+++ emacs/maps.el
@@ -1,3 +1,83 @@
+; Emacs
+(define-key evil-normal-state-map [f1] 'menu-bar-mode) ; toggle the menu bar
+(define-key evil-normal-state-map ",w" 'save-buffer) ; save
+(define-key evil-normal-state-map ",W" ":w!") ; force save
+(define-key evil-normal-state-map ",q" 'vimlike-quit) ; close current elscreen, or current window if only one elscreen
+(define-key evil-normal-state-map ",Q" ":q!") ; close current window, *even if modified*
+(define-key evil-normal-state-map ",d" 'kill-this-buffer) ; kill current buffer without confirmation
+(define-key evil-normal-state-map ",D" 'kill-this-buffer-volatile) ; kill current buffer without confirmation, *even if modified*
+(define-key evil-normal-state-map ",x" 'save-buffers-kill-emacs) ; save and quit
+(define-key evil-normal-state-map ",u" 'undo-tree-visualize) ; see undo history in tree format (this will be opened in a new split window)
+(define-key evil-insert-state-map [S-insert]
+	(lambda () (interactive) (insert (x-selection 'PRIMARY)))) ; paste X primary
+(define-key evil-normal-state-map "gw" 'fill-paragraph) ; insert hard line breaks
+
+
+; navigation
+; simulate vim's "nnoremap <space> 10jzz"
+(define-key evil-normal-state-map " "
+	(lambda ()
+		(interactive)
+		(next-line 10)
+		(evil-scroll-line-to-center nil)
+	)
+)
+; simulate vim's "nnoremap <backspace> 10kzz"
+(define-key evil-normal-state-map (kbd "DEL")
+	(lambda ()
+		(interactive)
+		(previous-line 10)
+		(evil-scroll-line-to-center nil)
+	)
+)
+(define-key evil-normal-state-map ",h"
+	(lambda () (interactive) (split-window-vertically) (balance-windows)))
+(define-key evil-normal-state-map ",v"
+	(lambda () (interactive) (split-window-horizontally) (balance-windows)))
+; move to next window
+(define-key evil-normal-state-map (kbd "TAB") 'other-window)
+; move to previous window
+(define-key evil-normal-state-map [backtab]
+	(lambda () (interactive) (other-window -1))
+)
+; Change K from being mapped to interactive man pages to being used as the
+; vanilla comma ',' key's functionality (intra-line backwards search repeat for
+; any t, T, f, F searches).
+(define-key evil-normal-state-map "K" 'evil-repeat-find-char-reverse)
+(define-key evil-visual-state-map "K" 'evil-repeat-find-char-reverse)
+; buffer movement
+(define-key evil-normal-state-map "H" 'evil-next-buffer)
+(define-key evil-normal-state-map "L" 'evil-prev-buffer)
+; new buffer
+(define-key evil-normal-state-map ",n" 'vimlike-:tabe)
+; find file
+(define-key evil-normal-state-map ",e" 'ido-find-file)
+; set line ending to UNIX
+(define-key evil-normal-state-map ",E"
+	(lambda () (interactive) (set-buffer-file-coding-system 'utf-8-unix t)))
+; replace all /r/n with just /n
+; make "kj" behave as ESC key, adapted from http://article.gmane.org/gmane.emacs.vim-emulation/980
+(define-key evil-insert-state-map "k" #'cofi/maybe-exit)
+
+; Elscreen
+; new vimlike "tab", aka "screen"
+(define-key evil-normal-state-map ",N" 'elscreen-create)
+; tab navigation
+(define-key evil-normal-state-map (kbd "C-l") 'elscreen-next)
+(define-key evil-normal-state-map (kbd "C-h") 'elscreen-previous)
+(define-key evil-insert-state-map (kbd "C-l") 'elscreen-next)
+(define-key evil-insert-state-map (kbd "C-h") 'elscreen-previous)
+
+; Nox integration (comment/uncomment regions)
+(define-key evil-visual-state-map ",c"
+	(lambda () (interactive) (my-addrem-comment t))) ; add comment
+(define-key evil-visual-state-map ",C"
+	(lambda () (interactive) (my-addrem-comment nil))) ; remove comment
+(define-key evil-normal-state-map ",c"
+  (lambda () (interactive) (my-addrem-comment t)))
+(define-key evil-normal-state-map ",C"
+  (lambda () (interactive) (my-addrem-comment nil)))
+
 ; org-mode
 (evil-define-key 'normal org-mode-map (kbd "M-o")
 	(lambda ()
