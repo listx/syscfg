8a1957c1dd456e290bfc1f7f6fc7f7c7e902e4c3
Linus Arver <linusarver@gmail.com>
2015-02-20 22:38:08 -0800

    emacs: org publish: indentation
    No functional change.

--- emacs/publish.el
+++ emacs/publish.el
@@ -2,15 +2,17 @@
 (setq org-src-preserve-indentation t)
 
 (setq org-publish-project-alist
-	'(("eh"
-		:base-directory "~/prog/elementary-haskell/"
-		:publishing-directory "~/prog/elementary-haskell/public_html"
-		:publishing-function org-html-publish-to-html
-		:section-numbers t
-		:with-toc t
-		:headline-levels 4
-		:html-preamble t
-		:html-head "<link rel=\"stylesheet\"
-			href=\"css/style.css\"
-			type=\"text/css\"/>"
-	)))
+	'(
+		("eh"
+			:base-directory "~/prog/elementary-haskell/"
+			:publishing-directory "~/prog/elementary-haskell/public_html"
+			:publishing-function org-html-publish-to-html
+			:section-numbers t
+			:with-toc t
+			:headline-levels 4
+			:html-preamble t
+			:html-head "<link rel=\"stylesheet\"
+				href=\"css/style.css\"
+				type=\"text/css\"/>"
+		)
+))
