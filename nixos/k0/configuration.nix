994ee7a47720ffab8e1091888d7c08c4d70387fa
Linus Arver <linusarver@gmail.com>
2015-03-20 06:25:45 +0000

    nixos: k0: tentative config

--- nixos/k0/configuration.nix
+++ nixos/k0/configuration.nix
@@ -1,83 +1,39 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      ../base.nix
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = ''
+    /dev/disk/by-id/ata-Samsung_SSD_840_EVO_250GB_S1DBNSAF368573R
+  '';
 
   boot.initrd.luks.devices = [
-{
-name = "luksroot";
-device = "/dev/sda2";
-preLVM = true;
-}
-];
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "lat9w-16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-    gnumake
-    rxvt_unicode
-    rxvt_unicode.terminfo
-    wget
+    {
+      name = "luksroot";
+      device = ''
+        /dev/disk/by-id/ata-Samsung_SSD_840_EVO_250GB_S1DBNSAF368573R-part2
+      '';
+      preLVM = true;
+    }
   ];
-  
-  programs.zsh.enable = true;
-  programs.zsh.promptInit = "";
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraGroups.l = { };
-  users.extraUsers.l = {
-    group = "l";
-    description = "Linus Arver";
-    createHome = true;
-    home = "/home/l";
-    extraGroups = [ "wheel" ];
-    shell = "/run/current-system/sw/bin/zsh";
-    uid = 1000;
+  networking = {
+    hostName = "k0";
+    interfaces = {
+      eno1 = {
+        ipAddress = "192.168.1.110";
+        prefixLength = 24;
+      };
+    };
   };
 
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+  };
 }
