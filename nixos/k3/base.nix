3a5f67c70ec7fbc68d773ae81e195c7831b1c2b8
Linus Arver <linusarver@gmail.com>
2015-03-19 21:46:19 -0700

    nixos: modularize k3 definition
    Interestingly, we don't have to create a symlink to base.nix from
    /etc/nixos --- this is probably a happy accident. As long as
    nixos-rebuild does not complain, it should be fine.
    
    Also remove redundant comments.

---
+++ nixos/k3/base.nix
@@ -0,0 +1,158 @@
+{ config, pkgs, ... }:
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    # Basic console tools
+    wget
+    curl
+    zsh
+    xlibs.xmodmap
+    rxvt_unicode
+    rxvt_unicode.terminfo
+    tmux
+    emacs
+    xsel
+    vim
+    silver-searcher
+    gnupg
+    mutt
+    htop
+
+    # Programming tools
+    git
+    tig
+    mercurial
+    gcc
+    gdb
+    gnumake
+    ruby
+    # For haskell development via `nix-shell`; run `sudo nix-channel --add
+    # <nixpkgs-unstable>`, then do `sudo nix-channel --update`.
+    # <nixpkgs-unstable> is located at
+    # https://nixos.org/channels/nixpkgs-unstable. The `haskellngPackages` set
+    # is only in nixpkgs-unstable as of 2014-02-06.
+    #
+    # It's OK to have multple NixOS channels at the same time. Nix takes care of
+    # dependencies without issue.
+    haskellngPackages.cabal2nix
+    # For invoking, e.g., `cabal2nix cabal://some-package`, because cabal2nix
+    # depends on `cabal update`. We also get `cabal repl` and other commands
+    # from the `cabal` binary with this package. The package
+    # 'haskellPackages.cabalInstall' has been renamed to
+    # 'haskellngPackages.cabal-install'.
+    haskellngPackages.cabal-install
+    # android development
+    androidsdk_4_4
+    android-udev-rules
+
+    # Programming libraries
+    glfw
+    glxinfo
+
+    # Browsers and multimedia
+    firefoxWrapper
+    chromium
+    aria2
+    rtorrent
+    geeqie
+    mpv
+    vlc
+    higan
+    cmus
+    evince
+    pidgin
+    texLiveFull
+  ];
+
+  # Fonts
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      baekmuk-ttf
+      corefonts
+      dejavu_fonts
+      ipafont
+      libertine
+      terminus_font
+      ubuntu_font_family
+    ];
+  };
+
+  hardware.pulseaudio.enable = true;
+
+  i18n = {
+    consoleFont = "lat9w-16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  networking.extraHosts = ''
+    192.168.1.100 forest
+    192.168.1.110 k0
+    192.168.1.114 k1
+    192.168.1.116 k3
+    192.168.1.120 ocean
+    74.207.246.114 l0
+  '';
+
+# allow installation of 'ati_unfree' video driver and also Firefox with Flash
+  nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.config.firefox = {
+    enableAdobeFlash = true;
+  };
+
+  # Create a /etc/zshenv and other things to make Zsh work properly. Among
+  # other things, this allows us to perform a "git pull <this machine's IP>"
+  # from a remote machine; without this, the login shell cannot find the
+  # git-upload-pack command, and the git pull operation will fail.
+  programs.zsh.enable = true;
+
+  # Delete things that come built-in by nix that customizes Zsh. For one thing,
+  # disable the prompt settings because they interfere with our own
+  # customizations.
+  programs.zsh.promptInit = "";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "altgr-intl";
+    xkbOptions = "terminate:ctrl_alt_bksp";
+    desktopManager.default = "none";
+    displayManager.sessionCommands = ''
+      # remap Caps_Lock key to xmonad's own, exclusive 'mod' key (no "sharing"
+      # with ALT or any other combination)
+      ${pkgs.xlibs.xmodmap}/bin/xmodmap -e "remove Lock = Caps_Lock"
+      ${pkgs.xlibs.xmodmap}/bin/xmodmap -e "add mod3 = Caps_Lock"
+      ${pkgs.xlibs.xmodmap}/bin/xmodmap ~/.xmodmap
+      ${pkgs.xlibs.xset}/bin/xset r rate 250 80
+    '';
+    displayManager.slim.defaultUser = "l";
+    windowManager.xmonad.enable = true;
+    windowManager.xmonad.enableContribAndExtras = true;
+    windowManager.default = "xmonad";
+  };
+
+  # Enable CUPS to print documents.
+  #services.printing.enable = true;
+
+  time.timeZone = "America/Los_Angeles";
+
+  users.extraGroups.l = { };
+  # Set password for extra users with 'passwd' command as root.
+  users.extraUsers.l = {
+    group = "l";
+    description = "Linus Arver";
+    createHome = true;
+    home = "/home/l";
+    extraGroups = [ "wheel" ] ;
+    shell = "/run/current-system/sw/bin/zsh";
+    uid = 1000;
+  };
+
+}
--- nixos/k3/configuration.nix
+++ nixos/k3/configuration.nix
@@ -1,22 +1,15 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+# See configuration.nix(5) manpage.
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      ./base.nix
       ./hardware-configuration.nix
     ];
 
-  # allow installation of 'ati_unfree' video driver and also Firefox with Flash
-  nixpkgs.config.allowUnfree = true;
-
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/disk/by-id/ata-ST9500420AS_5VJ59T8M";
   boot.loader.grub.extraEntries = ''
     menuentry "Windows7" {
@@ -39,18 +32,9 @@
   # connection won't work. The command `ping www.google.com` won't work. To fix
   # this, manually invoke `systemctl restart network-addresses-wlp3s0.service`.
   networking = {
-    extraHosts = ''
-      192.168.1.100 forest
-      192.168.1.110 k0
-      192.168.1.112 k2
-      192.168.1.114 k1
-      192.168.1.116 k3
-      192.168.1.120 ocean
-      74.207.246.114 l0
-    '';
-    hostName = "k3"; # Define your hostname.
+    hostName = "k3";
     hostId = "518ab295";
-    wireless.enable = true;  # Enables wireless.
+    wireless.enable = true;
     interfaces = {
       wlp3s0 = {
         ipAddress = "192.168.1.116";
@@ -62,138 +46,8 @@
     nameservers = [ "192.168.1.254" ];
   };
 
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "lat9w-16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  users.extraGroups.l = { };
-  # Set password for extra users with 'passwd' command as root.
-  users.extraUsers.l = {
-    group = "l";
-    description = "Linus Arver";
-    createHome = true;
-    home = "/home/l";
-    extraGroups = [ "wheel" ] ;
-    shell = "/run/current-system/sw/bin/zsh";
-    uid = 1000;
-  };
-
-  nixpkgs.config.firefox = {
-    enableAdobeFlash = true;
-  };
-
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    # Basic console tools
-    wget
-    curl
-    zsh
-    xlibs.xmodmap
-    rxvt_unicode
-    rxvt_unicode.terminfo
-    tmux
-    emacs
-    xsel
-    vim
-    silver-searcher
-    gnupg
-    mutt
-    htop
-
-    # Programming tools
-    git
-    tig
-    mercurial
-    gcc
-    gdb
-    gnumake
-    ruby
-    # For haskell development via `nix-shell`; run `sudo nix-channel --add
-    # <nixpkgs-unstable>`, then do `sudo nix-channel --update`.
-    # <nixpkgs-unstable> is located at
-    # https://nixos.org/channels/nixpkgs-unstable. The `haskellngPackages` set
-    # is only in nixpkgs-unstable as of 2014-02-06.
-    #
-    # It's OK to have multple NixOS channels at the same time. Nix takes care of
-    # dependencies without issue.
-    haskellngPackages.cabal2nix
-    # For invoking, e.g., `cabal2nix cabal://some-package`, because cabal2nix
-    # depends on `cabal update`. We also get `cabal repl` and other commands
-    # from the `cabal` binary with this package. The package
-    # 'haskellPackages.cabalInstall' has been renamed to
-    # 'haskellngPackages.cabal-install'.
-    haskellngPackages.cabal-install
-
-    # Programming libraries
-    glfw
-    glxinfo
-
-    # Browsers and multimedia
-    firefoxWrapper
-    chromium
-    aria2
-    rtorrent
-    geeqie
-    mpv
-    vlc
-    higan
-    cmus
-    evince
-    pidgin
-    texLiveFull
-  ];
-
-  # Create a /etc/zshenv and other things to make Zsh work properly. Among
-  # other things, this allows us to perform a "git pull <this machine's IP>"
-  # from a remote machine; without this, the login shell cannot find the
-  # git-upload-pack command, and the git pull operation will fail.
-  programs.zsh.enable = true;
-
-  # Delete things that come built-in by nix that customizes Zsh. For one thing,
-  # disable the prompt settings because they interfere with our own
-  # customizations.
-  programs.zsh.promptInit = "";
-
-  # Enable pulseaudio.
-  hardware.pulseaudio.enable = true;
-
-  # Fonts
-  fonts = {
-    enableFontDir = true;
-    enableGhostscriptFonts = true;
-    fonts = with pkgs; [
-      baekmuk-ttf
-      corefonts
-      dejavu_fonts
-      ipafont
-      libertine
-      terminus_font
-      ubuntu_font_family
-    ];
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-  # Enable the X11 windowing system.
   services.xserver = {
-    enable = true;
     videoDrivers = [ "ati" ];
-#    videoDrivers = [ "ati_unfree" ];
-    layout = "us";
-    xkbVariant = "altgr-intl";
-    xkbOptions = "terminate:ctrl_alt_bksp";
     synaptics = {
       enable = true;
       twoFingerScroll = true;
@@ -209,19 +63,6 @@
         Option "TapButton3" "0"
       EndSection
     '';
-    windowManager.xmonad.enable = true;
-    windowManager.xmonad.enableContribAndExtras = true;
-    windowManager.default = "xmonad";
-    desktopManager.default = "none";
-    displayManager.sessionCommands = ''
-      # remap Caps_Lock key to xmonad's own, exclusive 'mod' key (no "sharing"
-      # with ALT or any other combination)
-      ${pkgs.xlibs.xmodmap}/bin/xmodmap -e "remove Lock = Caps_Lock"
-      ${pkgs.xlibs.xmodmap}/bin/xmodmap -e "add mod3 = Caps_Lock"
-      ${pkgs.xlibs.xmodmap}/bin/xmodmap ~/.xmodmap
-      ${pkgs.xlibs.xset}/bin/xset r rate 250 80
-    '';
-    displayManager.slim.defaultUser = "l";
   };
 
 }
