aded63e02fd3ab18acf0fcfaf2865721518581f2
Linus Arver <linusarver@gmail.com>
2015-05-05 20:22:16 -0700

    gtag: clarify usage

--- script/sys/gtag.sh
+++ script/sys/gtag.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
 # Git tagging script
 
+# Usage: gtag <PROGRAM-NAME> <VERSION-NUMBER>
+
+# Note: <VERSION-NUMBER> should only be the version number itself; this script
+# will add "v" as a prefix when tagging it with git.
+
 msg=$1
 
 if [[ -z "$msg" ]]; then
