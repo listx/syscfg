9d5ecfaba517b95c54dc780cd7ec7a10f5d7884e
Linus Arver <linusarver@gmail.com>
2015-05-30 14:26:30 -0700

    nixos: w0: basic setup
    Unable to use XMonad key from Mac host at this point (only real
    problem).

--- nixos/w0/configuration.nix
+++ nixos/w0/configuration.nix
@@ -1,66 +1,38 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+# See configuration.nix(5) manpage.
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      ../base.nix
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/disk/by-id/ata-VBOX_HARDDISK_VBf13324d7-d759fdcb";
 
-  networking.hostName = "w0"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = "w0";
+    hostId =  "2650ce1d";
 
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "lat9w-16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  # environment.systemPackages = with pkgs; [
-  #   wget
-  # ];
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.l = {
-    group = "l";
-    description = "Linus Arver";
-    createHome = true;
-    home = "/home/l";
-    extraGroups = [ "wheel" ];
-    shell = "/run/current-system/sw/bin/bash";
-    uid = 1000;
   };
 
+  services.xserver = {
+    synaptics = {
+      enable = true;
+      twoFingerScroll = true;
+    };
+    config = ''
+      Section "InputClass"
+        Identifier "touchpad catchall"
+        Driver "synaptics"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Option "TapButton1" "0"
+        Option "TapButton2" "0"
+        Option "TapButton3" "0"
+      EndSection
+    '';
+  };
 }
