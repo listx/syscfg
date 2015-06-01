60104f3fcc43a860405dca410b6aa98b7e6bbcff
Linus Arver <linusarver@gmail.com>
2015-06-01 00:09:41 -0700

    nixpkgs: modularize config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,46 +1,54 @@
-# Taken from
-# http://lists.science.uu.nl/pipermail/nix-dev/2015-January/015601.html.
+super:
 {
+  # Nix evaluates this file and will call the `packageOverrides` attribute as a
+  # *function* and given the original Nixpkgs list of packages as a parameter
+  # (actually, the attribute within the passed-in-parameter, called `pkgs`,
+  # contains the actual packages).
+
+  # For an excellent reference on all keywords/syntax on the Nix language,
+  # consult the paper "NixOS: A Purely Functional Linux Distribution" (2008);
+  # despite its name, it actually defines in great detail the Nix language
+  # itself used to build NixOS.
+
+  # Another great resource is
+  # https://nixos.org/nixos/manual/sec-configuration-syntax.html, which has a
+  # "Syntax Summary" table.
+
+  # A more complete reference is
+  # http://nixos.org/nix/manual/#chap-writing-nix-expressions.
+
   packageOverrides = super: let self = super.pkgs; in
   {
-    haskellngPackages = super.haskellngPackages.override {
-      overrides = self: super: {
+    # We import the packages from `pkgsets`. But `pkgsets` doesn't define things
+    # on its own; rather, it picks out the official packages already defined in
+    # Nixpkgs' `default.nix` which in turn pulls in
+    # `pkgs/top-level/all-packages.nix`. So we have to pass into it the master
+    # list of packages, which is `self`. Then from within that file, we can say
+    # things like "vim" and Nix will know what "vim" means.
+    pkgsets = import ./pkgsets.nix { super = super; };
+    # `inherit` is a shorter syntax that basically does mass assignment of
+    # attributes for us. Otherwise, we'd have to write, e.g.,
+    #
+    #   self.l_base_env = l_base_env;
+    #   self.l_dev_env = l_dev_env;
+    #
+    # . Read more at http://nixos.org/nix/manual/#chap-writing-nix-expressions.
 
-        # Enable profiling. Taken from
-        # http://lists.science.uu.nl/pipermail/nix-dev/2015-January/015620.html.
-        mkDerivation = expr: super.mkDerivation (expr // {
-          enableLibraryProfiling = true; });
-
-        # Local packages not found on Hackage, but which still exist on the
-        # local machine. As long as they have a .cabal file, we can use
-        # `cabal2nix` to generate a Nix expression to build them.
-        #
-        # To generate the local package's nix expression, simply invoke
-        #
-        #   cabal2nix path/to/folder/containing/cabal/file > project-name.nix
-        #
-        # . E.g., like this:
-        #
-        #   cabal2nix ~/prog/ztile > ztile.nix
-        #
-        # . The `~/prog/ztile` folder contains the `ztile.cabal` file, which
-        # will be looked up automatically by cabal2nix. Then, it's simply a
-        # matter of moving the generated file to a folder inside ~/.nixpkgs,
-        # and calling that file with `callPackage`.
-        #
-        # As for the local package itself, you can do
-        #
-        #   cabal2nix --shell path/to/folder > shell.nix
-        #
-        # to create a nix-shell environment. You can just do `nix-shell` after
-        # that to get ghci. For cabal2nix to work properly, you need to have
-        # the `cabal` binary available. To do this, you should have
-        # `haskellngPackages.cabal-install` installed either on your system, or
-        # through nix-env.
-        auca = self.callPackage ./haskell/auca.nix {};
-        ztile = self.callPackage ./haskell/ztile.nix {};
-        timetracker = self.callPackage ./haskell/timetracker.nix {};
-      };
-    };
+    inherit (self.pkgsets)
+      l_set_home
+      l_set_work
+      l_set_base
+      l_set_dev
+      l_set_media
+      l_set_web
+      l_set_av
+      l_set_office
+      l_set_games
+      l_set_misc
+      l_set_haskell
+      # Haskell packages are hidden by default. To search for, e.g., "auca"
+      # defined from pkgsets.nix, do `nix-env -qaP -A
+      # nixpkgs.haskellngPackages.auca`
+      haskellngPackages;
   };
 }
---
+++ nixpkgs/pkgsets.nix
@@ -0,0 +1,222 @@
+let
+  HEAD = import /home/l/prog/foreign/nixpkgs {};
+in
+{super}:
+# The `with` keyword in `with X; Y` adds the attributes of X to the scope
+# while evaluating Y. This way, we can just write `buildEnv` instead of
+# `pkgs.buildEnv`.
+
+# `rec` keyword simply defines a recursive set instead of a plain set
+# (attributes can be defined in terms of each other in a recursive manner).
+with super; rec {
+  l_set_home = buildEnv {
+    name = "l-set-home";
+    paths = [
+      l_set_base
+      l_set_dev
+      l_set_media
+      l_set_haskell
+    ];
+  };
+
+  l_set_work = buildEnv {
+    name = "l-set-work";
+    paths = [
+      l_set_base
+      l_set_dev
+      l_set_haskell
+      l_set_web
+      l_set_av
+      l_set_office
+    ];
+  };
+
+  l_set_base = buildEnv {
+    name = "l-set-base";
+    paths = [
+      # Basic console tools
+      wget
+      curl
+      zsh
+      xlibs.xmodmap
+      xlibs.xev
+      rxvt_unicode
+      rxvt_unicode.terminfo
+      tmux
+      emacs
+      xsel
+      vim
+      silver-searcher
+      colordiff
+      gnupg
+      mutt
+      htop
+      dhcpcd
+      utillinuxCurses
+      lsof
+
+      # Docs
+      manpages
+      pthreadmanpages
+      stdmanpages
+
+      # Archiving
+      p7zip
+      unzip
+      lzma
+      unrar
+    ];
+  };
+
+  l_set_dev = buildEnv {
+    name = "l-set-dev";
+    paths = [
+      # Programming tools
+      git
+      tig
+      mercurial
+      clang
+      gcc
+      gdb
+      gnumake
+      ruby
+      # android development
+      androidsdk_4_4
+      android-udev-rules
+
+      # Programming libraries
+      boehmgc
+      glfw
+      glxinfo
+      pcg_c
+    ];
+  };
+
+  l_set_media = buildEnv {
+    name = "l-set-media";
+    paths = [
+      l_set_web
+      l_set_av
+      l_set_office
+      l_set_games
+      l_set_misc
+    ];
+  };
+
+  l_set_web = buildEnv {
+    name = "l-set-web";
+    paths = [
+      firefoxWrapper
+      chromium
+      links
+      aria2
+      rtorrent
+    ];
+  };
+
+  l_set_av = buildEnv {
+    name = "l-set-av";
+    paths = [
+      scrot
+      geeqie
+      gimp
+      mpv
+      ffmpeg
+      flac
+      shntool
+      vlc
+      cmus
+    ];
+  };
+
+  l_set_office = buildEnv {
+    name = "l-set-office";
+    paths = [
+      evince
+    ];
+  };
+
+  l_set_games = buildEnv {
+    name = "l-set-games";
+    paths = [
+      higan
+      sdlmame
+    ];
+  };
+
+  l_set_misc = buildEnv {
+    name = "l-set-misc";
+    paths = [
+      pidgin
+      texLiveFull
+    ];
+  };
+
+  # Taken from
+  # http://lists.science.uu.nl/pipermail/nix-dev/2015-January/015601.html. We
+  # redefine the default 'haskellngPackages' set, to enable profiling. We also
+  # add in some custom Haskell packages.
+  haskellngPackages = super.haskellngPackages.override {
+    overrides = self: super: {
+      # Enable profiling. Taken from
+      # http://lists.science.uu.nl/pipermail/nix-dev/2015-January/015620.html.
+      mkDerivation = expr: super.mkDerivation (expr // {
+        enableLibraryProfiling = true; });
+
+      # Local packages not found on Hackage, but which still exist on the
+      # local machine. As long as they have a .cabal file, we can use
+      # `cabal2nix` to generate a Nix expression to build them.
+      #
+      # To generate the local package's nix expression, simply invoke
+      #
+      #   cabal2nix path/to/folder/containing/cabal/file > project-name.nix
+      #
+      # . E.g., like this:
+      #
+      #   cabal2nix ~/prog/ztile > ztile.nix
+      #
+      # . The `~/prog/ztile` folder contains the `ztile.cabal` file, which
+      # will be looked up automatically by cabal2nix. Then, it's simply a
+      # matter of moving the generated file to a folder inside ~/.nixpkgs,
+      # and calling that file with `callPackage`.
+      #
+      # As for the local package itself, you can do
+      #
+      #   cabal2nix --shell path/to/folder > shell.nix
+      #
+      # to create a nix-shell environment. You can just do `nix-shell` after
+      # that to get ghci. For cabal2nix to work properly, you need to have
+      # the `cabal` binary available. To do this, you should have
+      # `haskellngPackages.cabal-install` installed either on your system, or
+      # through nix-env.
+      auca = self.callPackage ./haskell/auca.nix {};
+      ztile = self.callPackage ./haskell/ztile.nix {};
+      timetracker = self.callPackage ./haskell/timetracker.nix {};
+    };
+  };
+
+  l_set_haskell = buildEnv {
+    name = "l-haskell-set";
+    paths = with haskellngPackages; [
+      # For haskell development via `nix-shell`; run `sudo nix-channel --add
+      # <nixpkgs-unstable>`, then do `sudo nix-channel --update`.
+      # <nixpkgs-unstable> is located at
+      # https://nixos.org/channels/nixpkgs-unstable. The `haskellngPackages` set
+      # is only in nixpkgs-unstable as of 2015-02-06.
+      #
+      # It's OK to have multple NixOS channels at the same time. Nix takes care
+      # of dependencies without issue.
+      cabal2nix
+      # For invoking, e.g., `cabal2nix cabal://some-package`, because cabal2nix
+      # depends on `cabal update`. We also get `cabal repl` and other commands
+      # from the `cabal` binary with this package. The package
+      # 'haskellPackages.cabalInstall' has been renamed to
+      # 'haskellngPackages.cabal-install'.
+      cabal-install
+
+      auca
+      ztile
+      timetracker
+    ];
+  };
+}
