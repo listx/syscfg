2ee128caa7830c3eb20ae51d0a4b2fcd092b19a2
Linus Arver <linusarver@gmail.com>
2015-06-07 23:15:46 -0700

    emacs: generic mode for Xdefaults

--- emacs/init.el
+++ emacs/init.el
@@ -67,6 +67,23 @@
 		(when (not (package-installed-p pkg))
 		(package-install pkg))))
 
+; http://www.emacswiki.org/emacs/GenericMode
+(require 'generic-x)
+
+(define-generic-mode
+	'xdefaults-mode
+	'("!") ; comments
+	'() ; static keywords e.g., ("if" "else" "return")
+	'(
+		; regexes for matching aginst font-lock
+		("^\\w+" . 'font-lock-type-face)
+		("[*.:]" . 'font-lock-builtin-face)
+	)
+	'("\\.Xdefaults$") ; files to activate this mode (FIXME: maybe move it to kakapo instead?)
+	nil ; other functions to call
+	"A mode for ~/.Xdefaults and ~/.Xresources files" ; docstring for this mode
+)
+
 (require 'evil)
 (evil-mode 1)
 
