12ba7b11351f8675a941b93cc610cfc74a617529
Linus Arver <linusarver@gmail.com>
2015-07-30 22:34:54 -0700

    emacs: dired mode bindings

--- emacs/maps.el
+++ emacs/maps.el
@@ -196,3 +196,8 @@
 	(define-key org-mode-map (kbd "<S-iso-lefttab>") nil)
 	)
 )
+
+; Dired mode.
+(evil-define-key 'normal dired-mode-map "H" 'evil-next-buffer)
+(evil-define-key 'normal dired-mode-map "L" 'evil-prev-buffer)
+(evil-define-key 'normal dired-mode-map "K" 'dired-up-directory)
