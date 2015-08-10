2d9131a4d790c9c3df8e0cebf8e899e666b6e2f6
Linus Arver <linusarver@gmail.com>
2015-08-09 20:43:56 -0700

    xmonad: prev workspace binding

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -199,6 +199,7 @@ myKeys hostname conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 	, ((modm .|. altMask,   xK_h),              moveTo Next $ diffGrpWS (-1))  -- go to previous WS Group (the "moveTo Next" part here ensures that, after getting our desired new WSGroup, that we always display the top WS of that Group
 	, ((modm .|. altMask,   xK_o),              action_o)    -- go to next hidden empty WS (if possible), in current Group, and spawn a new "window" there (depends on group)
 	, ((modm, xK_n),                            moveTo Next $ nonEmptyWSExceptGrps [Net2, Sys])    -- go to any non-empty WS, except those WS belonging to the given WS Groups (for making sure that our "desk" is clean before we log off/shutdown)
+	, ((modm .|. shiftMask, xK_n),              moveTo Prev $ nonEmptyWSExceptGrps [Net2, Sys])
 	, ((modm .|. altMask .|. shiftMask, xK_j),  shiftTo Next grpWS) -- move window to next WS, in current Group
 	, ((modm .|. altMask .|. shiftMask, xK_k),  shiftTo Prev grpWS) -- move window to prev WS, in current Group
 	, ((modm .|. altMask .|. shiftMask, xK_l),  shiftTo Next $ diffGrpWS 1) -- move window to next WS group
