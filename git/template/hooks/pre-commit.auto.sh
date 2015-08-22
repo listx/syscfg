e6a49fb8f6e818c3ea6ccd63728f11021f32a646
Linus Arver <linusarver@gmail.com>
2015-08-22 02:57:00 -0700

    git: pre-commit: auto: trailing blank line removal

--- git/template/hooks/pre-commit.auto.sh
+++ git/template/hooks/pre-commit.auto.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-# This script fixes trailing whitespace and files without final newlines in the
-# index as well as the working tree. This is a conservative script that fixes
-# only those lines that were added into the index.
+# This script fixes files in the index and working tree based on the following
+# three criteria:
+#
+#   (1) lack of final newlines,
+#   (2) trailing whitespace, and
+#   (3) trailing blank lines at EOF,
+#
+# in that order. All three categories of errors are fixed only if they are in
+# the staging area. Any such errors elsewhere in the repository are untouched.
+# Trailing whitespace is only corrected on those specific lines added into the
+# staging area; i.e., such lines that already exist from a previous commit are
+# left alone.
+#
+# The last category (trailing blank lines of a file) is the only category
+# treated aggressively. Adding a trailing blank line to the staging area
+# triggers the deletion of *all* such blank lines in the file.
+
+# We define "newline" as the traditional UNIX line feed character (0x0a).
 
 set -e
 
@@ -12,57 +27,55 @@ function print_hash()
 	eval "declare -A hash="${hash_definition#*=}
 	for key in "${!hash[@]}"; do
 		echo -n "$key"
-		if [ "$2" == full ]; then
-			echo " ${hash[$key]}"
-		else
-			echo
-		fi
 	done | sort
 }
 
+errors_bl=0
 errors_nl=0
 errors_tw=0
 noeol="^\\\ No newline at end of file"
-declare -A tw nl
-
-# Detect files without a newline at the end.
-errors_nl=$(($(git diff --cached | grep "$noeol" | wc -l)))
-
-# Detect trailing whitespace.
-errors_tw=$(($(git diff --check --cached \
-	| grep -v "new blank line at EOF." \
-	| wc -l)))
+declare -a bl nl
+declare -A tw
 
 # Mark files without a final newline.
-if ((errors_nl)); then
-	while read file; do
-		if (($(git status --porcelain "$file" | grep -v "^D" | wc -l))) \
-			&& (($(git diff --cached "$file" \
-				| tail -n1 | grep "$noeol" | wc -l))); then
-			nl[$file]="x"
-		fi
-	done < <(git diff --cached --name-only)
-fi
+while read file; do
+	if (($(git status --porcelain "$file" | grep -v "^D" | wc -l))) \
+		&& (($(git diff --cached "$file" \
+			| tail -n1 | grep "$noeol" | wc -l))); then
+		nl+=("$file")
+		errors_nl=1
+	fi
+done < <(git diff --cached --name-only)
 
 # Mark files that have trailing whitespace.
-if ((errors_tw)); then
-	while read line; do
-		file=$(echo $line | cut -d: -f1)
-		lnum=$(echo $line | cut -d: -f2)
-		mode=$(git ls-files --stage | cut -d " " -f1)
-		if [ -z "${tw[$file]}" ]; then
-			tw[$file]=$lnum
-		else
-			tw[$file]+=" $lnum"
-		fi
-	done < <(git diff --check --cached \
-		| grep -v 'new blank line at EOF\.' \
-		| sed -e '/^+/d' -e 's/: trailing whitespace\.$//' -e 's/^/  /')
+while read line; do
+	file=$(echo $line | cut -d: -f1)
+	lnum=$(echo $line | cut -d: -f2)
+	mode=$(git ls-files --stage | cut -d " " -f1)
+	if [ -z "${tw[$file]}" ]; then
+		tw[$file]=$lnum
+	else
+		tw[$file]+=" $lnum"
 	fi
+	errors_tw=1
+done < <(git diff --check --cached \
+	| grep 'trailing whitespace\.$' \
+	| sed -e '/^+/d')
+
+# Mark files that have unnecessary trailing blank lines. By default, Git does
+# not mark all newly-added trailing blank lines; i.e., a file may have 100 newly
+# added trailing blank lines, but Git will only complain on the first such line.
+while read line; do
+	file=$(echo $line | cut -d: -f1)
+	bl+=("$file")
+	errors_bl=1
+done < <(git diff --check --cached \
+	| grep "new blank line at EOF\.$" \
+	| sed -e '/^+/d')
 
 # Fix all files which need to be fixed for the final newline.
 if ((errors_nl)); then
-	print_hash nl | while read file; do
+	for file in ${nl[*]}; do
 		mode=$(git ls-files --stage $file | cut -d " " -f1)
 		sha1=$((cat $file && echo) | git hash-object -w --stdin)
 		# Fix index.
@@ -90,5 +103,25 @@ if ((errors_tw)); then
 		git update-index --cacheinfo $mode,$sha1,$file
 		# Fix working tree.
 		perl -pi -e "s/[\t ]+$// if ($bools)" $file
+	done
+fi
+
+# Fix all files which need to be fixed for one or more trailing blank lines. It
+# could be the case that a new trailing blank line was added on top of
+# already-committed, existing blank lines. We delete these lines as well. This
+# is the only portion of this script that is "intrusive" in the sense that we
+# are touching lines that were *not* explicitly added to the index by the user.
+#
+# Because we first trim all trailing whitespace above, we only concern ourselves
+# with consecutive newline characters.
+if ((errors_bl)); then
+	for file in ${bl[*]}; do
+		mode=$(git ls-files --stage $file | cut -d " " -f1)
+		sha1=$(perl -0777 -p -e "s/\n+\z/\n/" $file \
+			| git hash-object -w --stdin)
+		# Fix index.
+		git update-index --cacheinfo $mode,$sha1,$file
+		# Fix working tree.
+		perl -0777 -pi -e "s/\n+\z/\n/" $file
 	done
 fi
