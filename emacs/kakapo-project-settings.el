7e8e39e39fe48e068142bb356a1d670904fac853
Linus Arver <linusarver@gmail.com>
2015-10-17 22:02:54 -0700

    emacs: .lhs: disable literate-haskell-mode-hook
    We use latex mode for Literate Haskell files.

--- emacs/kakapo-project-settings.el
+++ emacs/kakapo-project-settings.el
@@ -236,7 +236,6 @@
 			((string-match "\\.lhs$" b)
 				(progn
 					(h 'latex-mode-hook t 4)
-					(h 'literate-haskell-mode-hook t 4)
 					(setq mmm-global-mode 't)
 					(setq mmm-submode-decoration-level 1)
 					(mmm-ify-by-class 'literate-haskell-latex)
