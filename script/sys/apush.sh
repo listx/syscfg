321596d16859f1f73d30a890d940628bbd1d9fe7
Linus Arver <linusarver@gmail.com>
2016-02-28 22:50:00 -0800

    apush: somewhat hacky argument processing
    It's not as clean as it could be (the "-p path" argument _must_ come
    first before the files), but it works for now.

--- script/sys/apush.sh
+++ script/sys/apush.sh
@@ -1,7 +1,34 @@
 #!/usr/bin/env zsh
 
-# push files to android device
+# Push files to android device.
+#
+# Usage: apush.sh [-p destination_folder] <FILES...>
+#
+# NOTE: the "-p" flag MUST come first, because of our hacky usage of the "argc"
+# variable to mark the beginning of "$@".
+
+argc=0
+OPTIND=1
+while getopts ":p:" opt; do
+    case "$opt" in
+    p)
+        dest_suffix="$OPTARG"
+        ((argc+=2))
+        ;;
+    *)  exit 1
+        ;;
+    esac
+done
+
+# Set default location.
+dest="/sdcard/Download"
+if [[ -n "$dest_suffix" ]]; then
+	dest="${dest}/${dest_suffix}"
+fi
+
+shift $argc
+echo $@
 
 for f in $@; do
-	adb push "$f" /sdcard/Download
+	adb push "$f" "$dest"
 done
