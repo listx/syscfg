listx/syscfg

commit 968e30506fe225c5ed17cc8d5c42c7a927a65251

emacs: update auto-generated custom-set-* stuff

emacs/init.el
@@ -189,7 +189,14 @@
 	(helm-ag (projectile-project-root)))
 ; Default search term for helm-ag.
 (custom-set-variables
- '(helm-ag-insert-at-point 'word))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(helm-ag-insert-at-point (quote word))
+ '(inhibit-startup-buffer-menu t))
 
 ; darken inactive windows
 (require 'hiwin)
@@ -939,20 +946,13 @@ otherwise, close current tab (elscreen)."
 )
 
 ; auto-generated stuff by emacs itself...
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(inhibit-startup-buffer-menu t)
- '(blink-cursor-mode nil)
- '(column-number-mode t))
+
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- `(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height ,(my-text-height) :width normal :foundry "xos4" :family (nth 0 my/font-collection))))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 91 :width normal :foundry "xos4" :family (nth 0 my/font-collection))))))
 
 ; Fix font setup. For some reason, if we define a function call for :family above, the initial load does not use that font. Callling set-face-attribute manually fixes the issue.
 (set-face-attribute

commit ee74ae881de66fd90852b822877447da0355fcef

emacs: fix startup text height
968e30506fe225c5ed17cc8d5c42c7a927a65251 mistakenly dropped the use of
(my-text-height).

See
http://stackoverflow.com/questions/10573219/interesting-error-with-emacs-lisp.

emacs/init.el
@@ -950,7 +950,7 @@ otherwise, close current tab (elscreen)."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 91 :width normal :foundry "xos4" :family (nth 0 my/font-collection))))))
+ `(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height ,(my-text-height) :width normal :foundry "xos4" :family (nth 0 my/font-collection))))))
 
 ; Fix font setup. For some reason, if we define a function call for :family above, the initial load does not use that font. Callling set-face-attribute manually fixes the issue.
 (set-face-attribute