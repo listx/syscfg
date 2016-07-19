6ddb39abecbbfe2dbe459ab61bf67f67d9f87960
Linus Arver <linusarver@gmail.com>
2016-07-18 23:06:55 -0700

    nixos: new 'bare.nix' configuration
    This is meant for bare-bones server-type machines.

---
+++ nixos/bare.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }:
+
+{
+  i18n = {
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  networking = {
+    extraHosts = ''
+      192.168.0.100 m0
+      192.168.0.110 k0
+      192.168.0.114 k1
+      192.168.0.116 k3
+      192.168.0.130 mac
+      192.168.0.131 w1
+      192.168.0.132 w0
+      74.207.246.114 l0
+    '';
+  };
+
+  # allow installation of 'ati_unfree' video driver and also Firefox with Flash
+  nixpkgs.config.allowUnfree = true;
+
+  # Create a /etc/zshenv and other things to make Zsh work properly. Among
+  # other things, this allows us to perform a "git pull <this machine's IP>"
+  # from a remote machine; without this, the login shell cannot find the
+  # git-upload-pack command, and the git pull operation will fail.
+  programs.zsh.enable = true;
+
+  # Delete things that come built-in by nix that customizes Zsh. For one thing,
+  # disable the prompt settings because they interfere with our own
+  # customizations.
+  programs.zsh.promptInit = "";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # We're going to use `gpg-agent` with SSH support --- so to avoid conflict,
+  # disable OpenSSH's ssh-agent.
+  programs.ssh.startAgent = false;
+
+  # NTP for automated system clock adjustments.
+  services.ntp.enable = true;
+
+  time.timeZone = "America/Los_Angeles";
+
+  users.extraGroups.l = {
+    gid = 1000;
+  };
+
+  # Set password for extra users with 'passwd' command as root.
+  users.extraUsers.l = {
+    group = "l";
+    description = "Linus Arver";
+    createHome = true;
+    home = "/home/l";
+    extraGroups = [ "wheel" ] ;
+    shell = "/run/current-system/sw/bin/zsh";
+    uid = 1000;
+  };
+}
