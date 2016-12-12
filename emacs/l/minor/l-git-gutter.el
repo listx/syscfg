ecc2f8d14612f6364f8bb1e6053c6c9e50bf8339
Linus Arver <linusarver@gmail.com>
2016-12-12 02:59:40 -0800

    emacs: git-gutter: auto-update in realtime every 2s

--- emacs/l/minor/l-git-gutter.el
+++ emacs/l/minor/l-git-gutter.el
@@ -5,7 +5,9 @@
 	(custom-set-variables
 	'(git-gutter:modified-sign " ")
 	'(git-gutter:added-sign " ")
-	'(git-gutter:deleted-sign " "))
+	'(git-gutter:deleted-sign " ")
+	'(git-gutter:update-interval 2)
+	)
 	(set-face-background 'git-gutter:added "lime green")
 	(set-face-background 'git-gutter:modified "purple")
 	(set-face-background 'git-gutter:deleted "red")
