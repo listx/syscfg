450ef2f0a53734142cedfae83656fef447f86f12
Linus Arver <linusarver@gmail.com>
2017-01-14 15:40:43 -0800

    emacs: use `use-package' for ledger-mode

--- emacs/l/major/l-ledger.el
+++ emacs/l/major/l-ledger.el
@@ -1,6 +1,8 @@
-(autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
-(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
-; Go to the lastest entries at the end.
-(add-hook 'ledger-mode-hook 'evil-goto-line)
+(use-package ledger-mode
+	:config
+	(autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
+	(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+	(add-to-list 'auto-mode-alist '("\\.hledger$" . ledger-mode))
+)
 
 (provide 'l-ledger)
