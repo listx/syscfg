7a7477ae04ce074848be098601a98c0e584dd592
Linus Arver <linusarver@gmail.com>
2017-03-08 14:03:29 -0800

    git/emacs: make git committing/rebasing a bit better
    The changes to l-evil-magit.el are there to prevent those packages from
    overwriting the vanilla git-rebase-todo buffer with information about customized
    keybindings (since we disable git-rebase-mode in
    8791ad6619563361152bea525bce69f02a9230cc, the keybindings that come with those
    modes don't apply any more).

--- emacs/l/minor/l-evil-magit.el
+++ emacs/l/minor/l-evil-magit.el
@@ -1,3 +1,10 @@
-(use-package evil-magit)
+(use-package evil-magit
+	:config
+	(defun l/nop (orig-fun &rest args)
+		"Ignore `orig-fun'."
+	)
+	(advice-add 'evil-magit-add-rebase-messages :around #'l/nop)
+	(advice-add 'git-rebase-mode-show-keybindings :around #'l/nop)
+)
 
 (provide 'l-evil-magit)
