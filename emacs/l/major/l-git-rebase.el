53d117dcea89cdc6e0e6118d4ffef1a2bea24890
Linus Arver <linusarver@gmail.com>
2017-03-08 14:16:05 -0800

    emacs: l-git-rebase: clean up comments

--- emacs/l/major/l-git-rebase.el
+++ emacs/l/major/l-git-rebase.el
@@ -1,44 +1,12 @@
 (add-hook 'git-rebase-mode-hook 'l/git-rebase-keymap)
 
-; git-rebase.el is provided by Magit.
+; git-rebase.el is provided by Magit, and is pretty awful.
 (defun l/git-rebase-keymap ()
-	"My keys for git-rebase mode."
+	"My secret for git-rebase mode: just treat it as text, the way it was meant
+to be used."
 	(interactive)
 	(toggle-read-only nil)
 	(text-mode)
-
-	; Commented out are my failed attempts at getting git-rebase mode to behave.
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "p" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "r" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "e" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "s" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "f" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "x" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "d" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "u" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "h" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "j" nil)
-;	(evil-magit-define-key evil-magit-state 'git-rebase-mode-map "k" nil)
-
-;	(define-key git-rebase-mode-map "p" nil)
-;	(define-key git-rebase-mode-map "r" nil)
-;	(define-key git-rebase-mode-map "e" nil)
-;	(define-key git-rebase-mode-map "s" nil)
-;	(define-key git-rebase-mode-map "f" nil)
-;	(define-key git-rebase-mode-map "x" nil)
-;	(define-key git-rebase-mode-map "d" nil)
-;	(define-key git-rebase-mode-map "u" nil)
-;	(define-key git-rebase-mode-map "h" nil)
-;	(define-key git-rebase-mode-map "j" nil)
-;	(define-key git-rebase-mode-map "k" nil)
-
-;	(evil-magit-revert)
 )
-
-;(with-eval-after-load "git-rebase"
-;	`(progn
-;		(l/git-rebase-keymap)
-;	)
-;)
 
 (provide 'l-git-rebase)
