4fb7f07ea43becca0d422bce7f3d7ca5ecec0adf
Linus Arver <linusarver@gmail.com>
2017-03-15 14:53:41 -0700

    emacs: python: enable flycheck-mode

--- emacs/l/major/l-python.el
+++ emacs/l/major/l-python.el
@@ -3,6 +3,8 @@
 (defun l/python-setup ()
 	; Disable Python <backspace> binding.
 	(define-key python-mode-map (kbd "<backspace>") nil)
+	; Check with pylint. Flycheck automatically tries pylint if it is installed.
+	(flycheck-mode)
 )
 
 (provide 'l-python)
