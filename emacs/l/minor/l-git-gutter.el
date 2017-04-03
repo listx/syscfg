e621a075e0b9ee3cce99da323988ed9aa88a82a5
Linus Arver <linusarver@gmail.com>
2017-04-02 18:51:31 -0700

    emacs: prefer setq over custom-set-variables

--- emacs/l/minor/l-git-gutter.el
+++ emacs/l/minor/l-git-gutter.el
@@ -2,12 +2,10 @@
 	:config
 	; Git diff +/- marks.
 	(global-git-gutter-mode +1)
-	(custom-set-variables
-	'(git-gutter:modified-sign " ")
-	'(git-gutter:added-sign " ")
-	'(git-gutter:deleted-sign " ")
-	'(git-gutter:update-interval 2)
-	)
+	(setq git-gutter:modified-sign " ")
+	(setq git-gutter:added-sign " ")
+	(setq git-gutter:deleted-sign " ")
+	(setq git-gutter:update-interval 2)
 	(set-face-background 'git-gutter:added "lime green")
 	(set-face-background 'git-gutter:modified "purple")
 	(set-face-background 'git-gutter:deleted "red")
