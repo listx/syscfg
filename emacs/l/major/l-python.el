36d4a75fb020007df0728557003e4dd0ef7ef8a8
Linus Arver <linusarver@gmail.com>
2017-04-10 13:32:37 -0700

    emacs: python: max line length to 79 chars

--- emacs/l/major/l-python.el
+++ emacs/l/major/l-python.el
@@ -6,6 +6,14 @@
 	; Check with both flake8 and pylint.
 	(flycheck-mode)
 	(flycheck-add-next-checker 'python-flake8 'python-pylint)
+	; Set max line length to 79 characters (from PEP8). (Although Emacs columns
+	; are 0-indexed, column-enforce-mode counts from 1, so we use 79 here and
+	; not 78.)
+	(setq column-enforce-column 79)
+	; We need to tell Emacs to do paragrah-filling at 79 caharacters
+	; (column-enforce-mode only highlights regions --- it does not change how
+	; paragraph filling is done).
+	(setq fill-column 79)
 )
 
 (provide 'l-python)
