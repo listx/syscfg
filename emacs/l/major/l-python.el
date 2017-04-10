f12013824f764c3e79111c317a3585b824876025
Linus Arver <linusarver@gmail.com>
2017-04-09 22:58:17 -0700

    emacs: python: use both flake8 and pylint as linters

--- emacs/l/major/l-python.el
+++ emacs/l/major/l-python.el
@@ -3,8 +3,9 @@
 (defun l/python-setup ()
 	; Disable Python <backspace> binding.
 	(define-key python-mode-map (kbd "<backspace>") nil)
-	; Check with pylint. Flycheck automatically tries pylint if it is installed.
+	; Check with both flake8 and pylint.
 	(flycheck-mode)
+	(flycheck-add-next-checker 'python-flake8 'python-pylint)
 )
 
 (provide 'l-python)
