9b23f231f73c0c289de77edee11e62ff70e43834
Linus Arver <linusarver@gmail.com>
2017-08-28 16:21:55 -0700

    emacs: sh-mode: reorder disabling of sh-posix-bash checker
    After an Emacs packages upgrade earlier today, it appears that we need to set
    this _before_ starting up flycheck-mode

--- emacs/l/major/l-sh.el
+++ emacs/l/major/l-sh.el
@@ -1,9 +1,9 @@
 (add-hook 'sh-mode-hook 'l/sh-setup)
 
 (defun l/sh-setup ()
-  ; Check with shellcheck. Flycheck uses shellcheck if it is installed.
-  (flycheck-mode)
   ; Disable checking for POSIX compliance.
-  (setq flycheck-disabled-checkers '(sh-posix-bash)))
+  (setq flycheck-disabled-checkers '(sh-posix-bash))
+  ; Check with shellcheck. Flycheck uses shellcheck if it is installed.
+  (flycheck-mode))
 
 (provide 'l-sh)
