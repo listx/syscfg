80567b673a8ddc6bd6c6d6f1280e7559eeb89714
Linus Arver <linusarver@gmail.com>
2017-11-24 15:32:10 -0800

    mac: codify xset equivalent

--- script/sys/keyboard-repeat-mac.sh
+++ script/sys/keyboard-repeat-mac.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# See
-# https://apple.stackexchange.com/questions/10467/how-to-increase-keyboard-key-repeat-rate-on-os-x.
+# References.
+# https://apple.stackexchange.com/questions/10467/how-to-increase-keyboard-key-repeat-rate-on-os-x
+# https://stackoverflow.com/questions/43868520/how-to-adjust-keyrepeat-and-repeatdelay-in-osx-sierra
 
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+# Based on our own testing, "InitialKeyRepeat" is how long we wait, and
+# "KeyRepeat" is the rate of keys that are repeated. For "InitialKeyRepeat", we
+# assume that each unit is equivalent to 15ms, based on the comment of 15 ==
+# 225ms.
+defaults write -g InitialKeyRepeat -float 16.67 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
