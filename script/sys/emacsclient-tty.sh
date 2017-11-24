878831ba1b95ce0768de5008b7e43b23297ac8f7
Linus Arver <linusarver@gmail.com>
2017-11-24 15:56:44 -0800

    git: editor: set fill-column to 72
    The default is 80 (which we set in emacs/l/l-misc.el), which is regarded
    as too long in git commit messages.

--- script/sys/emacsclient-tty.sh
+++ script/sys/emacsclient-tty.sh
@@ -3,4 +3,4 @@
 # The (find-file ...) avoids showing "*scratch*" buffer on startup when invoking
 # from emacsclient.
 emacsclient --alternate-editor "" --tty \
-    --eval '(find-file "'"$1"'")'
+    --eval '(progn (find-file "'"$1"'") (setq fill-column 72))'
