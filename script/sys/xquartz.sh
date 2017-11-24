31e31dcbe894087460d4f084ad00d8455bd40a1f
Linus Arver <linusarver@gmail.com>
2017-11-24 15:33:54 -0800

    mac: xquartz: disable running xterm on startup
    It's a bit sad that such an annoying setting is buried inside some obscure
    setting.

---
+++ script/sys/xquartz.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# References.
+# https://apple.stackexchange.com/questions/53734/how-do-i-prevent-x11-opening-an-xterm-when-it-starts.
+defaults write org.macosforge.xquartz.X11 app_to_run "$HOME/syscfg/script/sys/terms/wb.sh"
