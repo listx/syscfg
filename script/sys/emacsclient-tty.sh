a45727a3bbf2980a3a21569eb1482fc395078d03
Linus Arver <linusarver@gmail.com>
2017-11-30 16:24:58 -0800

    emacs: when committing, move point to tho beginning of the buffer
    Recall that we use emacsclient for writing commit messages. It appeared
    before that sometimes when we commit, point would not appear at the
    beginning of the buffer. This was because the COMMIT_EDITMSG buffer (as
    a file) would get reused across multiple commits and, as Emacs thought
    that it was the same buffer, it would restore point in the same location
    as it was before (as if we were switching focus between windows back and
    forth). The fix is to simply force Emacs to move point to the beginning
    upon invoking the editor (in emacsclient-tty.sh).

--- script/sys/emacsclient-tty.sh
+++ script/sys/emacsclient-tty.sh
@@ -3,4 +3,4 @@
 # The (find-file ...) avoids showing "*scratch*" buffer on startup when invoking
 # from emacsclient.
 emacsclient --alternate-editor "" --tty \
-    --eval '(progn (find-file "'"$1"'") (setq fill-column 72))'
+    --eval '(progn (find-file "'"$1"'") (setq fill-column 72) (beginning-of-buffer))'
