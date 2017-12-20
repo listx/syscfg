0a85478d9d79627ccfdfeb53cc1b719b4cf42d28
Linus Arver <linusarver@gmail.com>
2017-12-20 10:46:30 -0800

    xmonad: enif: do not swap mouse keys
    We've gone ahead and modified the mouse EEPROM (using the official mouse
    drivers) to be a left-handed mouse, and no longer need this
    customization.

--- xmonad/init.sh
+++ xmonad/init.sh
@@ -3,9 +3,6 @@
 # initialize keyboard in a sane manner
 ~/syscfg/script/sys/initkeys.sh
 
-# Change mouse to be left-handed.
-xmodmap -e "pointer = 3 2 1"
-
 # Start ssh-agent.
 if [[ -z "$SSH_AUTH_SOCK" ]]; then
     eval $(ssh-agent)
