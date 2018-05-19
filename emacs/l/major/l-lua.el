21e0e39408b770a085e58815791f3b36079107e1
Linus Arver <linusarver@gmail.com>
2018-05-19 12:12:00 -0700

    emacs: lua: use use-package

--- emacs/l/major/l-lua.el
+++ emacs/l/major/l-lua.el
@@ -1,4 +1,6 @@
-(setq auto-mode-alist (cons '("\.lua$" . lua-mode) auto-mode-alist))
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(use-package lua-mode
+  :config
+  (setq auto-mode-alist (cons '("\.lua$" . lua-mode) auto-mode-alist))
+  (autoload 'lua-mode "lua-mode" "Lua editing mode." t))
 
 (provide 'l-lua)
