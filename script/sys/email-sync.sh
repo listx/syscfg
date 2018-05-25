6044c1798c9dc1c29893e7dd9e1ccac54c275665
Linus Arver <linusarver@gmail.com>
2018-05-25 14:31:56 -0700

    email-sync.sh: add interval between syncs

--- script/sys/email-sync.sh
+++ script/sys/email-sync.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
 offlineimap -a main & pid1=$!
 
 wait $pid1
 echo "Synced at $(date)"
+
+if [[ -n ${1:-} ]]; then
+    sleep $1
+fi
