9d9f7b5d71faad2858b715a3c3a0ac21ba987cfd
Linus Arver <linusarver@gmail.com>
2018-07-08 20:14:10 -0700

    email-sync: actually make use of the sleep interval
    The old code was completely broken; the calls to offlineimap themselves
    were looping (and not the script itself) because autorefresh was enabled
    in offlineimaprc. So, the call to sleep never even ran.

--- script/sys/email-sync.sh
+++ script/sys/email-sync.sh
@@ -4,11 +4,16 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-offlineimap -a main & pid1=$!
+while true; do
+    offlineimap -o -a main & pid1=$!
 
-wait $pid1
-echo "Synced at $(date)"
+    wait $pid1
+    echo
+    echo "EMAIL-SYNC: Synced at $(date)"
 
-if [[ -n ${1:-} ]]; then
-    sleep $1
-fi
+    if [[ -n ${1:-} ]]; then
+        echo
+        echo "Sleeping for $1 until next sync."
+        sleep $1
+    fi
+done
