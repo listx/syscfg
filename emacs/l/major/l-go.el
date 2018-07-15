db2cd334eeac334c0da32aa972ad3289695962a9
Linus Arver <linusarver@gmail.com>
2018-07-14 19:36:29 -0700

    emacs: go: use fill-paragraph instead of evil-fill
    This fixes comments not being properly hard-wrapped with evil-fill in
    go-mode (for some reason evil-fill does not work properly in go-mode).

--- emacs/l/major/l-go.el
+++ emacs/l/major/l-go.el
@@ -4,9 +4,25 @@
   (add-hook 'go-mode-hook 'l/go-setup))
 
 (defun l/go-setup ()
+  (advice-add 'evil-fill :around #'l/fill-region)
   ; FlyCheck will automatically pick up the "golint" tool if it is installed.
   (flycheck-mode)
   ; Run gofmt upon save.
   (add-hook 'before-save-hook #'gofmt-before-save))
+
+(defun l/fill-region (orig-fun &rest args)
+  ;(message "evil-fill called with args %S" args)
+  (let*
+    (
+      (use-fill-paragraph (equal major-mode 'go-mode))
+      (res
+        (if use-fill-paragraph
+          (fill-paragraph)
+          (apply orig-fun args))))
+    ;(message
+    ;  (concat
+    ;    (if use-fill-paragraph "fill-paragrpah" "evil-fill")
+    ;    " returned %S") res)
+    res))
 
 (provide 'l-go)
