61d7c93141574b70e4a7fc9d8f56bb39669b2963
Linus Arver <linusarver@gmail.com>
2018-07-24 20:07:38 -0700

    emacs: python: flycheck: try to use virtualenv environment

--- emacs/l/major/l-python.el
+++ emacs/l/major/l-python.el
@@ -1,10 +1,25 @@
 (add-hook 'python-mode-hook 'l/python-setup)
 
+; Based on
+; https://stackoverflow.com/questions/31443527/how-can-i-make-flycheck-use-virtualenv.
+; Depends on modifying Python's sys.path in .pylintrc as in
+; https://stackoverflow.com/a/39207275/437583 for this to work.
+(defun set-flychecker-executables ()
+  "Configure virtualenv for flake8 and lint."
+  (when (executable-find "flake8")
+  (flycheck-set-checker-executable (quote python-flake8)
+                                 (executable-find "flake8")))
+  (when (executable-find "flake8")
+  (flycheck-set-checker-executable (quote python-pylint)
+                                 (executable-find "pylint"))))
+
 (defun l/python-setup ()
   ; Disable Python <backspace> binding.
   (define-key python-mode-map (kbd "<backspace>") nil)
   ; Check with both flake8 and pylint.
   (flycheck-add-next-checker 'python-flake8 'python-pylint)
+  (add-hook 'flycheck-before-syntax-check-hook #'set-flychecker-executables
+    'local)
   ; Start Flycheck.
   (flycheck-mode)
   ; Set max line length to 79 characters (from PEP8). (Although Emacs columns
