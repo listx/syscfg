5da9866d46e829695e78ad30fae26e0f842fa891
Linus Arver <linusarver@gmail.com>
2019-05-27 11:15:17 +0200

    emacs: bazel: treat dash as word character

--- emacs/l/major/l-bazel.el
+++ emacs/l/major/l-bazel.el
@@ -1,8 +1,12 @@
 ; When https://github.com/bazelbuild/emacs-bazel-mode is ready, deprecate this.
 (use-package bazel-mode
   :config
+  (add-hook 'bazel-mode-hook 'l/bazel-setup)
   (add-to-list 'auto-mode-alist '(".*/BUILD\\'" . bazel-mode))
   (add-to-list 'auto-mode-alist '(".*/BUILD\\.bazel\\'" . bazel-mode))
   (add-to-list 'auto-mode-alist '(".*/WORKSPACE\\'" . bazel-mode)))
+
+(defun l/bazel-setup ()
+  (modify-syntax-entry ?- "w"))
 
 (provide 'l-bazel)
