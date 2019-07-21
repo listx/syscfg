f6e149682b7efb775bd0fa499d05871d406c1ff9
Linus Arver <linusarver@gmail.com>
2019-07-21 11:37:21 -0700

    xmonad: use more explicit commit hash
    Also, for whatever reason the old nixos-19.03 channel's Nix expressions
    had a problem where if we used it, the xmonad binary would not get
    linked against libXinerama.

--- xmonad/shell.nix
+++ xmonad/shell.nix
@@ -1,8 +1,14 @@
 # https://stackoverflow.com/a/56180220/437583
 
 with import (builtins.fetchGit {
-    url = https://github.com/NixOS/nixpkgs-channels;
-    ref = "nixos-19.03";
+    # Unfortunately, we can't check out arbitrary GitHub commit SHAs, so we
+    # have to refer to a local clone of
+    # https://github.com/NixOS/nixpkgs-channels (which itself just follows
+    # https://github.com/NixOS/nixpkgs.git with a verification delay, as per
+    # https://nixos.wiki/wiki/Nix_channels).
+    url = "/home/l/prog/foreign/nixpkgs";
+    # Known good commit for moving branch "nixpkgs-unstable".
+    ref = "104f8a0e1a17a894b320f86add409d9aebb36fe4";
 }) {};
 
 haskell.lib.buildStackProject {
