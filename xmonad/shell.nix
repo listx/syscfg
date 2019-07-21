bc753378c5f3a11e1eab70f3eca3546d2f577ce4
Linus Arver <linusarver@gmail.com>
2019-07-20 21:10:29 -0700

    xmonad: use custom (simpler) shell.nix
    Instead of using
    
        cabal2nix --shell . > shell.nix
    
    we instead use a custom shell.nix file that pulls in the Nix packages at
    a particular channel. Although the channel name "19.03" will keep
    moving, it is fine because it has been tested upstream. We could use a
    SHA as an argument to fetchGit, but it is unnecessary.
    
    This has the advantage of letting stack download all of the latest
    Haskell packages (instead of relying on the pre-built Nix ones). This
    makes more sense because if we're using the stack LTS resolver, we might
    as well try to defer as much to stack's package database as possible
    (instead of using Nixified versions as per cabal2nix).
    
    Also update LTS to 13.29 (GHC 8.6.5).
    
    The tip to use a custom shell.nix is from
    https://stackoverflow.com/a/56180220/437583.

--- xmonad/shell.nix
+++ xmonad/shell.nix
@@ -1,32 +1,15 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+# https://stackoverflow.com/a/56180220/437583
 
-let
+with import (builtins.fetchGit {
+    url = https://github.com/NixOS/nixpkgs-channels;
+    ref = "nixos-19.03";
+}) {};
 
-  inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, array, base, containers, random, safe, stdenv
-      , unix, X11, xmonad, xmonad-contrib
-      }:
-      mkDerivation {
-        pname = "xmonad-linus";
-        version = "0.0.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [
-          array base containers random safe unix X11 xmonad xmonad-contrib
-        ];
-        license = stdenv.lib.licenses.bsd2;
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+haskell.lib.buildStackProject {
+    name = "my-project";
+    buildInputs = [
+      haskell.compiler.ghc865
+      pkgconfig
+      x11
+      xorg.libXScrnSaver
+      xorg.libXrandr ]; }
