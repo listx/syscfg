d050e6b64fb5a2c68863760a7bae389824916d5f
Linus Arver <linusarver@gmail.com>
2019-08-12 07:25:47 -0700

    xmonad: shell.nix: use relative path to nixpkgs

--- xmonad/shell.nix
+++ xmonad/shell.nix
@@ -6,7 +6,7 @@ with import (builtins.fetchGit {
     # https://github.com/NixOS/nixpkgs-channels (which itself just follows
     # https://github.com/NixOS/nixpkgs.git with a verification delay, as per
     # https://nixos.wiki/wiki/Nix_channels).
-    url = "/home/l/prog/foreign/nixpkgs";
+    url = "~/prog/foreign/nixpkgs";
     # Known good commit for moving branch "nixpkgs-unstable".
     ref = "104f8a0e1a17a894b320f86add409d9aebb36fe4";
 }) {};
