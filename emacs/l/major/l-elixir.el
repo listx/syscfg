28876e58426fe08977cb1e4ecfb1d133437696ee
Linus Arver <linusarver@gmail.com>
2019-09-11 13:35:39 -0700

    emacs: elixir: add credo checker

--- emacs/l/major/l-elixir.el
+++ emacs/l/major/l-elixir.el
@@ -1,10 +1,13 @@
 (use-package elixir-mode
   :config
+  (use-package flycheck-credo)
   (use-package flycheck-elixir)
   (add-hook 'elixir-mode-hook 'l/elixir-setup))
 
 (defun l/elixir-setup ()
   (flycheck-mode)
+  (flycheck-credo-setup)
+  (flycheck-add-next-checker 'elixir 'elixir-credo)
   ; Format when saving.
   (add-hook 'before-save-hook 'elixir-format nil t))
 
