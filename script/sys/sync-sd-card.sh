f5b53689fc5ff84bab81ff5e08301be97f918e50
Linus Arver <linusarver@gmail.com>
2019-12-30 11:07:05 -0800

    sync-sd-card: parallelize image conversion to jpg
    Also choose which FUJI folder to sync/convert.

--- script/sys/sync-sd-card.sh
+++ script/sys/sync-sd-card.sh
@@ -4,13 +4,21 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if (( $# != 1 )); then
+	echo >&2 "usage: $0 <FUJI_FOLDER_PREFIX>"
+fi
+
+if [[ -z $1 ]]; then
+	echo >&2 "usage: $0 <FUJI_FOLDER_PREFIX>"
+fi
+
 if [[ -d /Volumes/sd128GB ]]; then
     rsync -ahP --no-whole-file --inplace /Volumes/{sd128GB,wd1TB}
 fi
 
 if [[ -d /Volumes/wd1TB ]]; then
-    raws=($(find /Volumes/wd1TB/sd128GB -name '*.RAF' | sort))
-    raw-to-jpg.sh \
+    mapfile -t raws < <(find "/Volumes/wd1TB/sd128GB/DCIM/${1}_FUJI" -name '*.RAF' | sort)
+    parallel -j "$(nproc --all)" -m raw-to-jpg.sh \
         ~/converted \
-        "${raws[@]}"
+        ::: "${raws[@]}"
 fi
