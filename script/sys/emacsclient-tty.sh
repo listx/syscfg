c3cda91c4b8c1a2699af3ec339ed362b9f0af3f4
Linus Arver <linusarver@gmail.com>
2020-02-20 16:00:38 -0800

    git/emacs: increase readability of commit message buffer

--- script/sys/emacsclient-tty.sh
+++ script/sys/emacsclient-tty.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env zsh
 
+read -r -d '' elisp << EOF
+(let
+  (
+  )
+  (find-file "$1")
+  (setq fill-column 72)
+  (beginning-of-buffer)
+  (load "elscreen" "ELScreen" t)
+)
+EOF
+
 # The (find-file ...) avoids showing "*scratch*" buffer on startup when invoking
 # from emacsclient.
 emacsclient --alternate-editor "" --tty \
-	--eval '(progn (find-file "'"$1"'") (setq fill-column 72) (beginning-of-buffer) (load "elscreen" "ELScreen" t))'
+	--eval "${elisp}"
