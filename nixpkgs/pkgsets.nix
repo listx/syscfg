e5166e43a1c38cc5f21e00355ea53f1523f92178
Linus Arver <linusarver@gmail.com>
2020-02-22 01:29:13 -0800

    nixpkgs: update nixpkgs-unstable ref

--- nixpkgs/pkgsets.nix
+++ nixpkgs/pkgsets.nix
@@ -7,7 +7,7 @@ let
     # https://nixos.wiki/wiki/Nix_channels).
     url = "/home/l/prog/foreign/nixpkgs";
     # Known good commit for moving branch "nixpkgs-unstable".
-    ref = "104f8a0e1a17a894b320f86add409d9aebb36fe4";
+    ref = "a21c2fa3ea2b88e698db6fc151d9c7259ae14d96";
   }) {};
 in
 {super}:
