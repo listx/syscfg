ac18bc3dc8615f943158b061b89f01e7705818f0
Linus Arver <linusarver@gmail.com>
2020-08-06 13:22:40 -0700

    emacs: reorg

--- emacs/init.el
+++ emacs/init.el
@@ -1,97 +1,14 @@
-; Call the debugger when an error is signaled and not handled.
-(setq debug-on-error t)
-; Work around a bug where esup tries to step into the byte-compiled version of
-; `cl-lib', and fails horribly.
-; See https://github.com/jschaf/esup/issues/54#issuecomment-651247749.
-(setq esup-depth 0)
-; Helper function for determining system type.
-(defun l/os (system)
-  (interactive)
-  (string-equal system-type system))
-
-; If on Mac, make Emacs aware of special paths, esp. Homebrew (for `exec-path').
-; Unfortunately, setting "PATH" does not automatically set `exec-path'. So, we
-; do both for consistency. See
-; http://ergoemacs.org/emacs/emacs_env_var_paths.html.
-(if (l/os "darwin")
-  (let*
-    ((HOME (getenv "HOME"))
-     (PATH (getenv "PATH"))
-     (paths `(
-      ,(concat HOME "/syscfg/script")
-      ,(concat HOME "/bin")
-      ,(concat HOME "/homebrew/bin"))))
-    (setenv "PATH" (concat (mapconcat 'identity paths ":") ":" PATH))
-    ; The way in which `exec-path' is set here is hacky. It appears that
-    ; exec-directory has little bearing to $PATH, so we just prepend $PATH to
-    ; exec-directory and hope for the best. It could be that exec-directory is
-    ; useless, but we give upstream code the benefit of the doubt.
-    (setq exec-path
-      (append
-        (split-string (getenv "PATH") ":")
-        (list exec-directory)))))
-
-(if (l/os "gnu/linux")
-  (let*
-    ((HOME (getenv "HOME"))
-     (PATH (getenv "PATH"))
-     (paths `(
-      ,(concat HOME "/syscfg/script")
-      ,(concat HOME "/.local/bin"))))
-    (setenv "PATH" (concat (mapconcat 'identity paths ":") ":" PATH))
-    (setq exec-path
-      (append
-        (split-string (getenv "PATH") ":")
-        (list exec-directory)))))
-
-; Increase garbage collection threshhold during startup to inhibit its
-; activation. This way, startup can be a little bit faster. See
-; https://github.com/nilcons/emacs-use-package-fast.
-(setq gc-cons-threshold 64000000)
-(add-hook 'after-init-hook #'(lambda ()
-  ;; restore after startup
-  (setq gc-cons-threshold 800000)))
-
-; For mac, increase initial frame size manually because we are not on XMonad.
-(when (l/os "darwin")
-  (add-to-list 'default-frame-alist '(height . 71))
-  (add-to-list 'default-frame-alist '(width . 80)))
-
-; Bootstrap `use-package'. See
-; http://www.lunaryorn.com/posts/my-emacs-configuration-with-use-package.html.
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives (append '(
-  ("melpa" . "https://melpa.org/packages/")
-  ("org" . "https://orgmode.org/elpa/")) package-archives))
-
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package)
-  (eval-when-compile (require 'use-package)))
-
-; Ensure that all packages handled by use-package are always installed if not
-; present.
-(setq use-package-always-ensure t)
-
-; Add custom load-paths.
+; Add load-paths.
 (add-to-list 'load-path "~/.emacs.d/l")
 (add-to-list 'load-path "~/.emacs.d/l/major")
 (add-to-list 'load-path "~/.emacs.d/l/minor")
 (add-to-list 'load-path "~/.emacs.d/l/theme")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/l/theme")
 
-; Macros.
-; See
-; http://emacs.stackexchange.com/questions/17420/how-to-pass-a-parameter-together-with-function.
-(defmacro l/define-key-args (map key func &rest args)
-  `(define-key ,map ,key (lambda () (interactive) (,func ,@args))))
+; Load things in alphabetical order where possible, but grouped by their
+; functionality.
 
-(defmacro l/add-hook-args (hook func &rest args)
-  `(add-hook ,hook (lambda () (interactive) (,func ,@args))))
-
-;; Load misc customizations.
+; Load misc customizations.
 (require 'l-misc)
 (require 'l-scratch)
 
@@ -124,7 +41,7 @@
 (require 'l-uim)
 (require 'l-vdiff)
 
-;; Load major-mode packages.
+; Load major-mode packages.
 (require 'l-bazel)
 (require 'l-c)
 (require 'l-c++)
@@ -163,7 +80,7 @@
 (require 'l-shen)
 (require 'l-yaml)
 
-;; Load things related to themes/appearance.
+; Load things related to themes/appearance.
 (require 'l-alect-themes)
 (require 'l-zenburn-theme)
 (require 'l-arjen-grey-theme)
--- emacs/l/l-misc.el
+++ emacs/l/l-misc.el
@@ -1,3 +1,89 @@
+; Call the debugger when an error is signaled and not handled.
+(setq debug-on-error t)
+; Work around a bug where esup tries to step into the byte-compiled version of
+; `cl-lib', and fails horribly.
+; See https://github.com/jschaf/esup/issues/54#issuecomment-651247749.
+(setq esup-depth 0)
+; Helper function for determining system type.
+(defun l/os (system)
+  (interactive)
+  (string-equal system-type system))
+
+; If on Mac, make Emacs aware of special paths, esp. Homebrew (for `exec-path').
+; Unfortunately, setting "PATH" does not automatically set `exec-path'. So, we
+; do both for consistency. See
+; http://ergoemacs.org/emacs/emacs_env_var_paths.html.
+(if (l/os "darwin")
+  (let*
+    ((HOME (getenv "HOME"))
+     (PATH (getenv "PATH"))
+     (paths `(
+      ,(concat HOME "/syscfg/script")
+      ,(concat HOME "/bin")
+      ,(concat HOME "/homebrew/bin"))))
+    (setenv "PATH" (concat (mapconcat 'identity paths ":") ":" PATH))
+    ; The way in which `exec-path' is set here is hacky. It appears that
+    ; exec-directory has little bearing to $PATH, so we just prepend $PATH to
+    ; exec-directory and hope for the best. It could be that exec-directory is
+    ; useless, but we give upstream code the benefit of the doubt.
+    (setq exec-path
+      (append
+        (split-string (getenv "PATH") ":")
+        (list exec-directory)))))
+
+(if (l/os "gnu/linux")
+  (let*
+    ((HOME (getenv "HOME"))
+     (PATH (getenv "PATH"))
+     (paths `(
+      ,(concat HOME "/syscfg/script")
+      ,(concat HOME "/.local/bin"))))
+    (setenv "PATH" (concat (mapconcat 'identity paths ":") ":" PATH))
+    (setq exec-path
+      (append
+        (split-string (getenv "PATH") ":")
+        (list exec-directory)))))
+
+; Increase garbage collection threshhold during startup to inhibit its
+; activation. This way, startup can be a little bit faster. See
+; https://github.com/nilcons/emacs-use-package-fast.
+(setq gc-cons-threshold 64000000)
+(add-hook 'after-init-hook #'(lambda ()
+  ;; restore after startup
+  (setq gc-cons-threshold 800000)))
+
+; For mac, increase initial frame size manually because we are not on XMonad.
+(when (l/os "darwin")
+  (add-to-list 'default-frame-alist '(height . 71))
+  (add-to-list 'default-frame-alist '(width . 80)))
+
+; Bootstrap `use-package'. See
+; http://www.lunaryorn.com/posts/my-emacs-configuration-with-use-package.html.
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives (append '(
+  ("melpa" . "https://melpa.org/packages/")
+  ("org" . "https://orgmode.org/elpa/")) package-archives))
+
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (eval-when-compile (require 'use-package)))
+
+; Ensure that all packages handled by use-package are always installed if not
+; present.
+(setq use-package-always-ensure t)
+
+; Macros.
+; See
+; http://emacs.stackexchange.com/questions/17420/how-to-pass-a-parameter-together-with-function.
+(defmacro l/define-key-args (map key func &rest args)
+  `(define-key ,map ,key (lambda () (interactive) (,func ,@args))))
+
+(defmacro l/add-hook-args (hook func &rest args)
+  `(add-hook ,hook (lambda () (interactive) (,func ,@args))))
+
 ; Disable "*GNU Emacs*" startup buffer.
 (setq inhibit-startup-screen t)
 
