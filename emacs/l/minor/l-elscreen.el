4b4737c1f15c46c513d79c97b8aceace38ad6319
Linus Arver <linusarver@gmail.com>
2020-08-05 22:30:56 -0700

    emacs: elscreen: remove unused GUI features

--- emacs/l/minor/l-elscreen.el
+++ emacs/l/minor/l-elscreen.el
@@ -1,6 +1,12 @@
 (use-package elscreen
   :config
   (load "elscreen" "ElScreen" t)
-  (elscreen-start))
+  (elscreen-start)
+  ; Hide elscreen numbers from the mode line.
+  (setq elscreen-display-screen-number nil)
+  ; Hide '[<->]' button for tab navigation.
+  (setq elscreen-tab-display-control nil)
+  ; Hide '[X]' button in the tab to kill the tab.
+  (setq elscreen-tab-display-kill-screen nil))
 
 (provide 'l-elscreen)
