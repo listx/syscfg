842db436bf1cd5cd89ea26f2d7632fd210c29597
Linus Arver <linusarver@gmail.com>
2020-08-23 16:31:39 -0700

    emacs: ledger-mode: reduce highlight behavior
    Instead of using a green background (annoying, because we'd have to change this
    color for each theme), just make the text bold.

--- emacs/l/major/l-ledger.el
+++ emacs/l/major/l-ledger.el
@@ -1,6 +1,7 @@
 (use-package ledger-mode
   :mode "\\.ledger\\'"
   :config
+  (set-face-attribute 'ledger-font-xact-highlight-face nil :weight 'bold :inherit nil)
   (autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
   (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode)))
 
