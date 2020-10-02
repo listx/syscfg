80450fdb712225d73c786a3ad9a77e063137922b
Linus Arver <linusarver@gmail.com>
2020-10-01 22:11:53 -0700

    add script for fixing windows 10 SSH permissions

---
+++ script/windows/set-ssh-authorized-keys-acl.ps1
@@ -0,0 +1,13 @@
+
+# Fix permissions for authorized_keys file.
+# https://www.concurrency.com/blog/may-2019/key-based-authentication-for-openssh-on-windows
+
+$acl = Get-Acl C:\ProgramData\ssh\administrators_authorized_keys
+$acl.SetAccessRuleProtection($true, $false)
+$administratorsRule = New-Object system.security.accesscontrol.filesystemaccessrule("Administrators","FullControl","Allow")
+$systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYSTEM","FullControl","Allow")
+$acl.SetAccessRule($administratorsRule)
+$acl.SetAccessRule($systemRule)
+$acl | Set-Acl
+
+Write-Host "Done"
