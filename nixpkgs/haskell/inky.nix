6a6a168ad67bd6a2600483d05927bf19a49c68dd
Linus Arver <linusarver@gmail.com>
2020-10-06 23:06:21 -0700

    nixpkgs: add inky package

---
+++ nixpkgs/haskell/inky.nix
@@ -0,0 +1,20 @@
+{ mkDerivation, base, colour, criterion, dhall, either, hpack
+, megaparsec, optparse-generic, stdenv, tasty, tasty-hspec, text
+}:
+mkDerivation {
+  pname = "inky";
+  version = "0.0.0";
+  src = /home/l/prog/inky;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base colour dhall either megaparsec optparse-generic text
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [ base ];
+  testHaskellDepends = [ base tasty tasty-hspec ];
+  benchmarkHaskellDepends = [ base criterion ];
+  prePatch = "hpack";
+  homepage = "https://github.com/githubuser/inky#readme";
+  license = stdenv.lib.licenses.mit;
+}
