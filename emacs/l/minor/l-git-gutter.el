94fb5f2abf7599af5378e48d28abcfde26ec84cd
Linus Arver <linusarver@gmail.com>
2020-12-08 16:53:26 -0800

    emacs: make git-gutter refresh more frequently
    Terminal emacs doesn't get the frame focus hook events, so we have to use a
    different hook. We just advise "select-window", because it is used every time we
    choose a window.
    
    This is slightly better than advising "other-window" which we have mapped to the
    TAB key in normal mode, because it covers the case where we switch tabs with C-h
    and C-l, which are not covered by "other-window". (This is just my theory, but
    it makes sense to me and has been observed in practice.)

--- emacs/l/minor/l-git-gutter.el
+++ emacs/l/minor/l-git-gutter.el
@@ -4,6 +4,15 @@
   (global-git-gutter-mode +1)
   ; Update git-gutter every time we lose/regain focus to the frame.
   (add-hook 'focus-in-hook 'git-gutter:update-all-windows)
+  ; Update git-gutter every time we lose/regain focus of the current window.
+  ; This is to catch cases where we are SSH-ed in to a machine and are running
+  ; emacs in terminal mode, which doesn't get the same "frame" focus signals as
+  ; above because there is literally no frame.
+  (advice-add 'select-window :around #'l/git-gutter-refresh)
+  (defun l/git-gutter-refresh (orig-fun &rest args)
+    (progn
+      (apply orig-fun args)
+      (git-gutter:update-all-windows)))
   (setq git-gutter:modified-sign " ")
   (setq git-gutter:added-sign " ")
   (setq git-gutter:deleted-sign " ")
