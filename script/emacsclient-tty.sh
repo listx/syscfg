a6edd8d9bf4f550b343d2e66029581bfd349ac24
Linus Arver <linusarver@gmail.com>
2020-12-11 15:34:37 -0800

    emacsclient scripts: be more verbose
    Also, use cat over 'read -r -d "" ...' syntax because it seems to be more
    portable.

--- script/emacsclient-tty.sh
+++ script/emacsclient-tty.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-read -r -d '' elisp << EOF
+set -euo pipefail
+
+elisp=$(cat << EOF
 (progn
+  ; Open up the git buffer.
   (find-file-other-tab "$1")
+  ; Set line length to 72, because commit message hygiene matters.
   (setq fill-column 72)
+  ; Go to the top of the buffer.
   (beginning-of-buffer)
 )
 EOF
+)
 
 # The (find-file ...) avoids showing "*scratch*" buffer on startup when invoking
 # from emacsclient.
-emacsclient --alternate-editor "" --tty \
-	--eval "${elisp}"
+emacsclient \
+    --alternate-editor "" \
+    --tty \
+    --eval "${elisp}"
