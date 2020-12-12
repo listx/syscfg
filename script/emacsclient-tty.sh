a8e8b607e8c71b77303f30497b48281169943408
Linus Arver <linusarver@gmail.com>
2020-12-11 17:32:25 -0800

    emacsclient: disable menu-bar-mode

--- script/emacsclient-tty.sh
+++ script/emacsclient-tty.sh
@@ -8,14 +8,17 @@ elisp=$(cat << EOF
   (find-file "$(readlink -e "${1}")")
   ; Set line length to 72, because commit message hygiene matters.
   (setq fill-column 72)
+  ; Disable menu (for some reason the menu is enabled on the frame
+  ; sometimes).
+  (menu-bar-mode 0)
   ; Go to the top of the buffer.
   (beginning-of-buffer)
 )
 EOF
 )
 
-# The (find-file ...) avoids showing "*scratch*" buffer on startup when invoking
-# from emacsclient.
+# The (find-file ...) avoids showing "*scratch*" buffer on startup when
+# invoking from emacsclient.
 emacsclient \
     --alternate-editor "" \
     --tty \
