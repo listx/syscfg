6852da06a4c796b52d93069e776fca267a637f77
Linus Arver <linusarver@gmail.com>
2021-05-06 18:51:09 -0700

    emacs: org: remove mmm-mode
    This is in preparation for using poly-org-mode instead. The main reason
    we are removing mmm-mode is because it does not play well with
    org-mode's indentation (org-indent-mode).

--- emacs/l/major/l-mmm-mode.el
+++ emacs/l/major/l-mmm-mode.el
@@ -21,48 +21,6 @@
 
   (mmm-add-mode-ext-class 'latex-mode "\\.lhs\\'" 'l/literate-haskell-code)
 
-  ; For mode names that match the 'lang' in '#+begin_src lang', we don't need
-  ; to provide an optional submode. But for those that don't match, we can do
-  ; it like this:
-  ;
-  ;   (my-mmm-org-auto-class "fortran" 'f90-mode)
-  ;   (my-mmm-org-auto-class "perl" 'cperl-mode)
-  ;   (my-mmm-org-auto-class "shell" 'shell-script-mode)
-  ;
-  ; Adopted from http://jblevins.org/log/mmm.
-  (defun l/mmm-org-auto-class (lang &optional submode)
-    "Define a mmm-mode class for LANG in `org-mode' using SUBMODE.
-    If SUBMODE is not provided, use `LANG-mode' by default."
-    (let
-      (
-        (class (intern (concat "org-my-mmm-" lang)))
-        (submode (or submode (intern (concat lang "-mode"))))
-        (front (concat "^\\#\\+begin_src " lang "\n"))
-        (back "^\\#\\+end_src$"))
-      (mmm-add-classes (list (list class :submode submode :front front :back back)))
-      (mmm-add-mode-ext-class 'org-mode nil class)))
-
-  ; Add subclasses for #+begin_src blocks in org-mode.
-  (mapc 'l/mmm-org-auto-class
-    '(
-    "awk"
-    "bibtex"
-    "c"
-    "cpp"
-    "css"
-    "haskell"
-    "html"
-    "latex"
-    "lisp"
-    "makefile"
-    "markdown"
-    "python"
-    "r"
-    "ruby"
-    "sql"
-    "stata"
-    "xml"))
-
   ; Re-fontify sub-mode portions when idle. The manual command for this is
   ; `mmm-parse-buffer'.
   (setq mmm-parse-when-idle 't))
