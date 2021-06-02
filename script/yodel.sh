61b3f30b0acea30ae3dd50af437bb09991c698d2
Linus Arver <linusarver@gmail.com>
2021-06-01 19:53:13 -0700

    yodel.sh: process with r128gain after downloading

--- script/yodel.sh
+++ script/yodel.sh
@@ -7,5 +7,7 @@ set -o pipefail
 # Given a list of youtube links, download them all.
 
 for link in $(cat "$1"); do
-	youtube-dl -x "$link"
+	f="$(youtube-dl --get-filename -o '%(title)s-%(id)s.opus' "$link")"
+	youtube-dl --extract-audio "$link"
+	r128gain "${f}"
 done
