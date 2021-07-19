418fb4b1d92ed1e646edfb6be5928ddfb7510cbe
Linus Arver <linusarver@gmail.com>
2021-07-19 02:09:21 -0700

    emacs: make terminal emacs focus changes trigger git-gutter refresh
    Before, the focus-in-hook would only work on graphical emacs. This
    solution works on terminal emacs as well.

--- emacs/l/minor/l-git-gutter.el
+++ emacs/l/minor/l-git-gutter.el
@@ -2,20 +2,20 @@
   :config
   ; Git diff +/- marks.
   (global-git-gutter-mode +1)
-  ; Update git-gutter every time we lose/regain focus to the frame.
-  (add-hook 'focus-in-hook 'git-gutter:update-all-windows)
   ; Update git-gutter every time we lose/regain focus of the current window.
   ; This is to catch cases where we are SSH-ed in to a machine and are running
   ; emacs in terminal mode, which doesn't get the same "frame" focus signals as
   ; above because there is literally no frame.
-  (advice-add 'select-window :around #'l/git-gutter-refresh)
   (defun l/git-gutter-refresh (orig-fun &rest args)
     (prog1
       (apply orig-fun args)
       (git-gutter:update-all-windows)))
+  (advice-add 'select-window :around #'l/git-gutter-refresh)
+  ; Update git-gutter every time we lose/regain focus to the frame. See
+  ; https://emacs.stackexchange.com/a/60971/13006.
+  (add-function :after after-focus-change-function (lambda () (unless (frame-focus-state) (git-gutter:update-all-windows))))
   (setq git-gutter:modified-sign " ")
   (setq git-gutter:added-sign " ")
-  (setq git-gutter:deleted-sign " ")
-  (setq git-gutter:update-interval 2))
+  (setq git-gutter:deleted-sign " "))
 
 (provide 'l-git-gutter)
