4c6ab9e98c6ce5725372642888cfb48727fc1aa1
Linus Arver <linusarver@gmail.com>
2021-08-08 01:23:36 -0700

    simplify_path.sh: make it understand named directories

--- script/simplify_path.sh
+++ script/simplify_path.sh
@@ -15,24 +15,100 @@ set -euxo pipefail
 
 path="${1:-}"
 
-if [[ "${path}" == "$HOME"* ]]; then
-	path="~${path#$HOME}"
+is_named_path=0
+
+# Recognize specially-named paths.
+named_paths_definition="$HOME/syscfg/zsh/path-aliases"
+named_paths=()
+while IFS= read -r line; do
+	# Skip empty lines.
+	if [[ -z "${line}" ]]; then
+		continue
+	fi
+	# Skip comment lines.
+	if [[ "${line}" == \#* ]]; then
+		continue
+	fi
+
+	path_expanded="${line#*=}"
+	# Convert literal "$HOME" to actual value of "$HOME".
+	if [[ "${path_expanded}" == "\$HOME"* ]]; then
+		path_expanded="${path_expanded/\$HOME/$HOME}"
+	fi
+	path_alias="${line%=*}"
+	path_alias="${path_alias#hash -d}"
+	# Remove leading whitespace characters. See
+	# https://stackoverflow.com/a/3352015/437583.
+	path_alias="${path_alias#"${path_alias%%[![:space:]]*}"}"
+	named_paths+=("${path_alias}=${path_expanded}")
+done < "${named_paths_definition}"
+
+# See if we hit any of the specially-named paths. If so, then we set final_path
+# to include it.
+for named_path in "${named_paths[@]}"; do
+	if [[ "${path}" != "${named_path#*=}"* ]]; then
+		continue
+	fi
+
+	# Grab the path alias name. E.g., "s" for ~/syscfg.
+	path_aliased="~${named_path%=*}"
+
+	# Determine how long the full expanded path is. E.g., for ~/syscfg it may be
+	# 14 characters for "/home/l/syscfg".
+	path_expanded_len="${named_path#*=}"
+	path_expanded_len="${#path_expanded_len}"
+
+	# For the "prefix" that can be truncated, make it start from the portion of
+	# the path that excludes the leading prefix that lines up with the expanded
+	# path. E.g., if path is set to "/home/l/syscfg/foo/bar", then we want the
+	# "prefix" to be just "foo/bar".
+	prefix="${path:${path_expanded_len}}"
+	# Remove leading '/', if any.
+	prefix="${prefix#/}"
+
+	# If the prefix has a slash in it (e.g., "foo/bar"), then we need to
+	# identify the last portion "bar" as the basename, and any leading bit as
+	# the prefix (which can be truncated).
+	if [[ "${prefix}" == */* ]]; then
+		basename="${prefix##*/}"
+		prefix="${prefix%/*}"
+		final_path="${path_aliased}/${prefix}/${basename}"
+	else
+		# This is a case where we're given just 1 child directory underneath the
+		# expanded alias. E.g., "/home/l/syscfg/foo". In this case there is
+		# nothing to truncate (both the path alias and the basename should be
+		# fully displayed), so the final_path lacks a "prefix" portion.
+		basename="${prefix}"
+		final_path="${path_aliased}/${basename}"
+	fi
+	is_named_path=1
+	break
+done
+
+if ! (( is_named_path )); then
+	# Convert expanded $HOME path to just "~".
+	if [[ "${path}" == "$HOME"* ]]; then
+		path="~${path#$HOME}"
+	fi
+	prefix="${path%/*}"
+	basename="${path##*/}"
+	final_path="${prefix}/${basename}"
 fi
 
-prefix="${path%/*}"
-basename="${path##*/}"
-
+# Truncate long prefix paths.
 __TRUNCATE_THRESHOLD=30
 
-# Truncate long prefix paths.
-final_path="${prefix}/${basename}"
 i=0
 mapfile -td / fields < <(printf "%s\0" "${prefix}")
 while (( "${#final_path}" > "${__TRUNCATE_THRESHOLD}" )) && (( $i < "${#fields[@]}" )); do
 	# Truncate this portion of the prefix to its first letter.
 	fields["${i}"]="${fields[${i}]::1}"
 	prefix=$(IFS=/; printf '%s' "${fields[*]}")
-	final_path="${prefix}/${basename}"
+	if (( is_named_path )); then
+		final_path="${path_aliased}/${prefix}/${basename}"
+	else
+		final_path="${prefix}/${basename}"
+	fi
 	i=$(($i + 1))
 done
 
@@ -42,4 +118,4 @@ if [[ "${final_path}" == "~/~" ]]; then
 	exit 0
 fi
 
-echo "${final_path}"
+echo "${final_path%/}"
