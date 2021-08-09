c5dc44dc57d49be5b4776819e4ad98ce741e30b0
Linus Arver <linusarver@gmail.com>
2021-08-08 21:38:44 -0700

    tmux_mouse_word_handler.sh: refactor
    For some reason different parts of this code fire. It seems like there's
    a race between what "#{mouse_word}" could be at any given moment. We're
    still trying to find out what is the cause behind the non-determinism,
    but for now this change at least makes the code easier to read.

--- script/tmux_mouse_word_handler.sh
+++ script/tmux_mouse_word_handler.sh
@@ -20,63 +20,27 @@ __pane_current_path="${1}"
 __mouse_word="${2}"
 __tmux_config_snippet=""
 
-# If the word starts with '/' or '~', just try to cd into it. We virtually press
-# a TAB key to resolve any named directories or shortened directory names
-# (because Zsh can recognize paths like "/a/b/c" to mean
-# "/apple/banana/carrot").
-if [[ "${__mouse_word}" =~ ^[/~]  ]]; then
+# "univ_open" is the function that "d" is aliased to in Zsh.
+__l_univ_open()
+{
+	__tmux_config_snippet=$(cat << EOF
+	send-keys -t "#{pane_id}" " d #{mouse_word}" Enter
+EOF
+	)
+	echo "${__tmux_config_snippet}"
+}
+
+__l_complete_and_univ_open()
+{
 	__tmux_config_snippet=$(cat << EOF
 	send-keys -t "#{pane_id}" " d #{mouse_word}" Tab Enter
 EOF
 	)
 	echo "${__tmux_config_snippet}"
-	exit
-fi
+}
 
-if [[ ! -e "${__pane_current_path}/${__mouse_word}" ]]; then
-	# If the word doesn't exist, check if it's of the form "file:NUM", and if
-	# "file" does exit, open it up at that line number.
-	if [[ "${__mouse_word}" =~ ^[^:]+:[0-9]+$ ]] \
-		&& [[ -e "${__pane_current_path}/${__mouse_word%:*}" ]]; then
-		__tmux_config_snippet=$(cat << EOF
-		display-popup -w90% -h90% -E -E -d '#{pane_current_path}' ' \
-			~/syscfg/script/emacsclient-tty.sh \
-				"${__mouse_word%:*}" \
-				"+${__mouse_word#*:}" \
-		'
-EOF
-	)
-		echo "${__tmux_config_snippet}"
-		exit
-	fi
-
-	# If the word is not a recognizable file from PWD, copy it in. This was
-	# derived from the default DoubleClick1Pane binding with vanilla Tmux.
-	__tmux_config_snippet=$(cat << EOF
-		if-shell -F "#{||:#{pane_in_mode},#{mouse_any_flag}}" "send -M" "copy-mode -H ; send -X select-word ; run -d0.3 ; send -X copy-pipe-and-cancel"
-EOF
-	)
-	echo "${__tmux_config_snippet}"
-	exit
-fi
-
-# If the word is interesting (it's a path that can be reached from PWD), then
-# either cd into it, or open it if it's not a child directory.
-if [[ -e "${__pane_current_path}/${__mouse_word}" ]]; then
-	if [[ -d "${__pane_current_path}/${__mouse_word}" ]]; then
-		# There's no need to quote #{mouse_word} here because if the user clicks
-		# on a directory with spaces in its name, only part of the directory
-		# name will get copied as "#{mouse_word}" anyway, defeating the "[[ -d
-		# ... ]]" check above.
-		__tmux_config_snippet=$(cat << EOF
-		send-keys -t "#{pane_id}" " d #{mouse_word}" Enter
-EOF
-		)
-		echo "${__tmux_config_snippet}"
-		exit
-	fi
-
-	# This is most likely a regular text file, so just open it with emacsclient.
+__l_edit()
+{
 	__tmux_config_snippet=$(cat << EOF
 		display-popup -w90% -h90% -E -E -d '#{pane_current_path}' ' \
 			~/syscfg/script/emacsclient-tty.sh "#{mouse_word}" \
@@ -84,5 +48,60 @@ EOF
 EOF
 	)
 	echo "${__tmux_config_snippet}"
-	exit
-fi
+}
+
+__l_edit_at_line()
+{
+	__tmux_config_snippet=$(cat << EOF
+	display-popup -w90% -h90% -E -E -d '#{pane_current_path}' ' \
+		~/syscfg/script/emacsclient-tty.sh \
+			"${__mouse_word%:*}" \
+			"+${__mouse_word#*:}" \
+	'
+EOF
+)
+	echo "${__tmux_config_snippet}"
+}
+
+__l_copy_to_tmux_buffer()
+{
+	__tmux_config_snippet=$(cat << EOF
+		if-shell -F "#{||:#{pane_in_mode},#{mouse_any_flag}}" "send -M" "copy-mode -H ; send -X select-word ; run -d0.3 ; send -X copy-pipe-and-cancel"
+EOF
+	)
+	echo "${__tmux_config_snippet}"
+}
+
+main()
+{
+	# If the word starts with '/' or '~', just try to cd into it. We virtually
+	# press a TAB key to resolve any named directories or shortened directory
+	# names (because Zsh can recognize paths like "/a/b/c" to mean
+	# "/apple/banana/carrot").
+	if [[ "${__mouse_word}" =~ ^[/~] ]]; then
+		__l_complete_and_univ_open
+	# If the word is interesting (it's a path that can be reached from PWD),
+	# then either cd into it, or open it if it's not a child directory.
+	elif [[ -d "${__pane_current_path}/${__mouse_word}" ]]; then
+		# There's no need to quote #{mouse_word} here because if the user
+		# clicks on a directory with spaces in its name, only part of the
+		# directory name will get copied as "#{mouse_word}" anyway,
+		# defeating the "[[ -d ... ]]" check above.
+		__l_univ_open
+	elif [[ -e "${__pane_current_path}/${__mouse_word}" ]]; then
+		# This is most likely a regular text file, so just open it with
+		# emacsclient.
+		__l_edit
+	# If the word doesn't exist, check if it's of the form "file:NUM", and if
+	# "file" does exit, open it up at that line number.
+	elif [[ "${__mouse_word}" =~ ^[^:]+:[0-9]+$ ]] \
+		&& [[ -e "${__pane_current_path}/${__mouse_word%:*}" ]]; then
+		__l_edit_at_line
+	# If the word is not a recognizable file from PWD, copy it in. This was
+	# derived from the default DoubleClick1Pane binding with vanilla Tmux.
+	else
+		__l_copy_to_tmux_buffer
+	fi
+}
+
+main "$@"
