84a44ca9d862bafb55c45a08dd7b442bb131a30b
Linus Arver <linusarver@gmail.com>
2021-08-15 19:21:12 -0700

    sync-sd-card.sh: exit early if no raws found

--- script/sync-sd-card.sh
+++ script/sync-sd-card.sh
@@ -17,6 +17,10 @@ fi
 
 if [[ -d /Volumes/wd1TB ]]; then
 	mapfile -t raws < <(find "/Volumes/wd1TB/sd128GB/DCIM/${1}_FUJI" -name '*.RAF' | sort)
+	if ! (( ${#raws[@]} )); then
+		echo >&2 "no *.RAF raws found in ${1}_FUJI"
+		exit
+	fi
 	parallel -j "$(nproc --all)" -m raw-to-jpg.sh \
 		~/converted \
 		::: "${raws[@]}"
