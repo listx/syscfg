c422acc4a5ac3a93c7b77b354962cdbc5cd1dcf5
Linus Arver <linusarver@gmail.com>
2021-08-29 10:42:17 -0700

    org-roam: fix org-roam-v2-ack declaration

--- emacs/l/minor/l-org-roam.el
+++ emacs/l/minor/l-org-roam.el
@@ -2,8 +2,9 @@
   :ensure t
   :hook
   (after-init . org-roam-db-autosync-mode)
+  :init
+  (setq org-roam-v2-ack t)
   :custom
-  (org-roam-v2-ack t)
   (org-roam-directory "~/lo/note"))
 
 (provide 'l-org-roam)
