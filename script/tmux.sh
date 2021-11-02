dd45073de1c66c4f0bd37959bc2ba32260678bb4
Linus Arver <linusarver@gmail.com>
2021-11-01 22:19:17 -0700

    scripts: tmux.sh: don't use tmuxinator
    We use tmux-continuum which autosaves tmux sessions automatically, so
    there's no need to use tmuxinator any more.

--- script/tmux.sh
+++ script/tmux.sh
@@ -4,11 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-tmuxinator_project_exists()
-{
-	tmuxinator list --newline | grep -v '^tmuxinator projects:' | grep -q "^${1}"
-}
-
 main()
 {
 	local host
@@ -36,20 +31,9 @@ main()
 	local host_session
 	host_session="${host}-${session}"
 
-	# Try to use a tmuxinator project if the desired tmux session name matches a
-	# tmuxinator project name. If a tmux session named "${host_session}" is
-	# already running, attach to it.
-	if tmuxinator_project_exists "${session}"; then
-		tmuxinator start \
-			--suppress-tmux-version-warning=SUPPRESS-TMUX-VERSION-WARNING \
-			--name="${host_session}" \
-			"${session}"
-	else
-		# If the desired tmux session name is not a tmuxinator project name,
-		# then just create a new raw tmux session. Attach to an existing session
-		# of the same name if it is already running.
-		tmux new-session -A -s "${host_session}"
-	fi
+	# Create a new raw tmux session. Attach to an existing session
+	# of the same name if it is already running.
+	tmux new-session -A -s "${host_session}"
 }
 
 main "$@"
