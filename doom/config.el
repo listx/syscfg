7c9273dc245b1e924717700fddac61c3cbb697a5
Linus Arver <linusarver@gmail.com>
2021-11-18 20:55:08 -0800

    doom: highlight-sexp-mode: only enable for lisps
    The main annoyance is that it does not highlight anything if point is on
    a comment or string. So we limit it to just lispy major modes.

--- doom/config.el
+++ doom/config.el
@@ -732,7 +732,7 @@ Also add the number of windows in the window configuration."
 
 ;; Highlight the entire expression when hovering over matching parentheses.
 (use-package! highlight-sexp
-  :hook ((prog-mode . highlight-sexp-mode))
+  :hook (lisp-mode emacs-lisp-mode)
   :config
   (custom-set-faces!
     '(hl-sexp-face :extend t))
