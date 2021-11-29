2796412fe2f3382d571361e86d28ef71ef1be3bc
Linus Arver <linusarver@gmail.com>
2021-11-29 11:00:06 -0800

    lhc: fix compilation on mac
    References:
    
    https://www.reddit.com/r/rust/comments/ptagz8/comment/hduxb0q
    https://www.reddit.com/r/NixOS/comments/knq819/comment/ghmedr1

--- lhc/shell.nix
+++ lhc/shell.nix
@@ -1,5 +1,9 @@
 { profile ? "default" }:
 with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
-mkShell {
-  nativeBuildInputs = [ rust-bin.stable.latest.${profile} pkg-config openssl ];
+let
+  platform-specific =
+    if stdenv.isDarwin then darwin.apple_sdk.frameworks.Security else { };
+in mkShell {
+  nativeBuildInputs =
+    [ rust-bin.stable.latest.${profile} pkg-config openssl platform-specific ];
 }
