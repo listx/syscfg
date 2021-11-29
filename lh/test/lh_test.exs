3c5be1394056e9f78788fedbd39c2d1ea3e320be
Linus Arver <linusarver@gmail.com>
2021-11-28 22:05:51 -0800

    lh: test: add TODO comment

--- lh/test/lh_test.exs
+++ lh/test/lh_test.exs
@@ -1,3 +1,8 @@
+# TODO: Add some basic tests for known inputs/outputs.
+# See
+# https://dev.to/jonlunsford/elixir-building-a-small-json-endpoint-with-plug-cowboy-and-poison-1826
+# for an idea on testing endpoints.
+
 defmodule LHTest do
   use ExUnit.Case
   doctest LH
