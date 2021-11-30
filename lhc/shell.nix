fa7ce6c289f13f1113043861878397b78f9aa1c6
Linus Arver <linusarver@gmail.com>
2021-11-29 23:40:42 -0800

    lhc: shell.nix: fix type error (again)
    Apparently we cannot quote the text
    "darwin.apple_sdk.frameworks.Security".

--- lhc/shell.nix
+++ lhc/shell.nix
@@ -1,8 +1,8 @@
 { profile ? "default" }:
 with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 let
-  platform-specific = lib.optionalString stdenv.hostPlatform.isDarwin
-    "darwin.apple_sdk.frameworks.Security";
+  platform-specific = if stdenv.hostPlatform.isDarwin then
+    darwin.apple_sdk.frameworks.Security else "";
 in mkShell {
   nativeBuildInputs =
     [ rust-bin.stable.latest.${profile} pkg-config openssl platform-specific ];
