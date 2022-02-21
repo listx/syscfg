c38d2d1d7e0a81f4758f7b6d9c9f04167bc7e4b1
Linus Arver <linusarver@gmail.com>
2022-02-21 14:36:53 -0800

    nixos: k0: use usb wifi adapter

--- nixos/k0/configuration.nix
+++ nixos/k0/configuration.nix
@@ -29,6 +29,19 @@
 
     defaultGateway = "192.168.0.1";
     nameservers = [ "8.8.8.8" ];
+
+    # Use the USB Wifi adapter "ALFA AWUS036ACHM". See
+    # https://github.com/morrownr/USB-WiFi/blame/73bf16e0621daafdbc9c852836ea0473616ec626/README.md#L61.
+    # This enables wireless support via /etc/wpa_supplicant.conf.
+    wireless.enable = true;
+    # Assign static IP manually. The other half of this trick to make it work
+    # is to tell the Motorola router to start its DHCP lease assignment address
+    # from 192.168.0.10+, and then use the addresses 192.168.0.2-192.168.0.9
+    # statically (192.168.0.1 is reserved for the router itself).
+    interfaces.wlp0s20u4u3.ipv4.addresses = [{
+      address = "192.168.0.9";
+      prefixLength = 24;
+    }];
   };
 
   # Enable cron service
