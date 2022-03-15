7be12d34aafb10a1876b7469da3c9023d091859f
Linus Arver <linusarver@gmail.com>
2022-03-15 14:13:28 -0700

    yodel: slightly improve user-friendliness

--- script/yodel.sh
+++ script/yodel.sh
@@ -7,6 +7,11 @@ set -o pipefail
 # Given a list of youtube links, download them all.
 
 for link in $(cat "$1"); do
+	# If the link is just a plain video ID, construct the link.
+	if [[ ! "${link}" =~ ^https://.* ]]; then
+		link="https://www.youtube.com/watch?v=${link}"
+	fi
+
 	f="$(youtube-dl --get-filename -o '%(title)s-%(id)s' "$link")"
 	youtube-dl --extract-audio "$link"
 	r128gain "${f}"*
