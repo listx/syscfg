e00e9f45c55a4a6436a29992d5843ba71864ae6f
Linus Arver <linusarver@gmail.com>
2022-03-24 02:24:03 -0700

    lhx: get /ping from lhc
    lhx is the Haskell version of lhc, and can GET against the toplevel
    "/ping" path.
    
    This is just a small learning exercise to understand how to use GHC
    9.2.1 and the Haskell servant-client library on NixOS.

--- lhx/lhx.hs
+++ lhx/lhx.hs
@@ -1,4 +1,28 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
+import Data.ByteString qualified as BS
+import Data.Proxy
+import Network.HTTP.Client (newManager, defaultManagerSettings)
+import Servant.API
+import Servant.Client
+
+type Message = BS.ByteString
+
+type MyApi = "ping" :> Get '[OctetStream] Message -- GET /ping
+
+myApi :: Proxy MyApi
+myApi = Proxy
+
+getPing :: ClientM Message
+getPing = client myApi
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+  manager' <- newManager defaultManagerSettings
+  res <- runClientM getPing (mkClientEnv manager' (BaseUrl Http "localhost" 8080 ""))
+  case res of
+    Left err -> putStrLn $ "Error: " ++ show err
+    Right msg -> BS.putStr msg
--- lhx/shell.nix
+++ lhx/shell.nix
@@ -7,12 +7,22 @@ let
     with ps;
     [
       # If you want to use cached Haskell packages (instead of making cabal
-      # download them dynamically), you can list them here (e.g., "mtl").
+      # download them dynamically), you can list them here (e.g., "mtl"). But
+      # this only works if the nixpkgs cache has them built; it may be that
+      # nixpkgs does not have them yet (at least at the correct versions) which
+      # would mean that we would have to download and build these packages
+      # ourselves.
+
+      #http-client
+      #servant
+      #servant-client
+      #text
     ]);
 in pkgs.stdenv.mkDerivation {
   name = "my-haskell-env-0";
   buildInputs = [
     ghc
+    pkgs.zlib
     # pkgs.gmp
     # pkgs.libffi
     # pkgs.pkgconfig
