d4a7f17a630228886d684d99b6ee22342f0e0f05
Linus Arver <linusarver@gmail.com>
2022-04-01 01:11:20 -0700

    lhx: add paths-sort support

--- lhx/lhx.hs
+++ lhx/lhx.hs
@@ -27,6 +27,7 @@ import LHX.GitVersion
 type Message = T.Text
 
 type LHAPI = "path-shorten" :> ReqBody '[JSON] PathShortenReqBody :> Post '[JSON] PathShortened -- POST /path-shorten
+        :<|> "paths-sort" :> ReqBody '[JSON] PathsSortReqBody :> Post '[JSON] PathsSorted -- POST /paths-sort
         :<|> "ping" :> GetNoContent -- GET /ping
         :<|> "shutdown" :> GetNoContent -- GET /shutdown
 
@@ -47,19 +48,35 @@ data PathShortened = PathShortened
 
 instance FromJSON PathShortened
 
+data PathsSortReqBody = PathsSortReqBody
+  { paths :: FilePath -- the $PATH to sort
+  , priorities_raw :: T.Text
+  , home :: T.Text -- $HOME
+  } deriving (Generic, Show)
+
+instance ToJSON PathsSortReqBody
+
+data PathsSorted = PathsSorted
+  { paths_sorted :: FilePath
+  } deriving (Generic, Show)
+
+instance FromJSON PathsSorted
+
 -- These are the client "action" functions. All we need to do is provide a type
 -- signature, and Servant generates the implementation. We can then pass in one
 -- of these actions depending on what we want to do in "runClient".
 postPathShorten :: PathShortenReqBody -> ClientM PathShortened
+postPathsSort :: PathsSortReqBody -> ClientM PathsSorted
 getPing :: ClientM NoContent
 getShutdown :: ClientM NoContent
-(postPathShorten :<|> getPing :<|> getShutdown) = client lhApi
+(postPathShorten :<|> postPathsSort :<|> getPing :<|> getShutdown) = client lhApi
 
 newtype Opts = Opts
   { subcommand :: Subcommand }
 
 data Subcommand
   = PathShorten PathShortenOpts
+  | PathsSort PathsSortOpts
   | Ping
   | Shutdown
 
@@ -68,12 +85,18 @@ data PathShortenOpts = PathShortenOpts
   , pathAliases :: FilePath
   }
 
+data PathsSortOpts = PathsSortOpts
+  { paths :: FilePath
+  , prioritiesRaw :: FilePath
+  }
+
 optionsP :: Parser Opts
 optionsP = Opts <$> subcommandP
 
 subcommandP :: Parser Subcommand
 subcommandP = hsubparser
   ( command "path-shorten" (info (PathShorten <$> pathShortenOptsP) (progDesc "Shorten a path"))
+  <> command "paths-sort" (info (PathsSort <$> pathsSortOptsP) (progDesc "Sort the $PATH"))
   <> command "ping" (info (pure Ping) (progDesc "Check lh server connectivity"))
   <> command "shutdown" (info (pure Shutdown) (progDesc "Shut down lh server instance"))
   <> metavar "SUBCOMMAND"
@@ -93,6 +116,23 @@ pathAliasesP = strOption
   <> help "file containing path aliases"
   )
 
+pathsSortOptsP :: Parser PathsSortOpts
+pathsSortOptsP
+  = PathsSortOpts
+  <$> (argument str (metavar "$PATH"))
+  <*> prioritiesRawP
+
+
+prioritiesRawP :: Parser FilePath
+prioritiesRawP = strOption
+  ( long "priorities-raw"
+  <> short 'p'
+  -- NOTE: adding "value" (default value) makes this option not mandatory.
+  <> value ""
+  <> metavar "FILE"
+  <> help "file containing path priorities (for sorting $PATH)"
+  )
+
 runClient :: ClientEnv -> ClientM a -> (a -> IO ()) -> IO ()
 runClient clientEnv action_ valHandler = do
   res <- runClientM action_ clientEnv
@@ -107,17 +147,28 @@ optsHandler (Opts subcommand) mgr = do
   let
     clientEnv = mkClientEnv mgr (BaseUrl Http "localhost" 8080 "")
     resolve = runClient clientEnv
+  home <- T.pack <$> getEnv "HOME"
   case subcommand of
-    PathShorten pso -> do
-      pa <- T.readFile pso.pathAliases
-      home <- T.pack <$> getEnv "HOME"
+    PathShorten o -> do
+      pa <- T.readFile o.pathAliases
       let
         pathShortenReqBody = PathShortenReqBody
-          { name = pso.name
+          { name = o.name
           , aliases_raw = pa
           , substitutions = H.fromList [("$HOME", home), (home, "~")]
           }
       resolve (postPathShorten pathShortenReqBody) (T.putStr . T.pack . (.path_shortened))
+    PathsSort o -> do
+      prio <- if null o.prioritiesRaw
+        then return ""
+        else T.readFile o.prioritiesRaw
+      let
+        pathsSortReqBody = PathsSortReqBody
+          { paths = o.paths
+          , priorities_raw = prio
+          , home = home
+          }
+      resolve (postPathsSort pathsSortReqBody) (T.putStr . T.pack . (.paths_sorted))
     Ping -> resolve getPing (\_ -> T.putStrLn "OK")
     Shutdown -> resolve getShutdown (\_ -> T.putStrLn "Shutting down...")
 
