f2550b26208f5cd34463e656dfe16a619a9afdea
Linus Arver <linusarver@gmail.com>
2022-04-03 13:28:04 -0700

    lhc: add paths-sort support

--- lhc/src/main.rs
+++ lhc/src/main.rs
@@ -15,6 +15,11 @@ struct PathShortened {
     path_shortened: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct PathsSorted {
+    paths_sorted: String,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let settings = Settings::new().unwrap();
 
@@ -41,10 +46,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .required(true),
                 ),
         )
+        .subcommand(
+            Command::new("paths-sort")
+                .about("Sort the $PATH")
+                .arg(
+                    Arg::new("paths_to_sort")
+                        .help("the paths to sort ($PATH)")
+                        .index(1)
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("priorities_raw_file")
+                        .long("priorities-raw")
+                        .value_name("FILE")
+                        .help("File containing path priorities (for sorting $PATH)")
+                        .takes_value(true)
+                        .required(true),
+                ),
+        )
         .subcommand(Command::new("ping").about("Check lh server connectivity"))
         .subcommand(Command::new("shutdown").about("Shut down lh server instance"));
     let matches = cmd.get_matches();
     let client = Client::new();
+    let request_base_url = format!("http://{}:{}", settings.server.domain, settings.server.port);
+    let home_dir = env::var("HOME").expect("$HOME not set");
     match matches.subcommand_name() {
         Some("path-shorten") => {
             if let Some(m) = matches.subcommand_matches("path-shorten") {
@@ -55,7 +80,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     fs::read_to_string(path_aliases_file).unwrap_or_default();
 
                 let mut subs = HashMap::new();
-                let home_dir = env::var("HOME").expect("$HOME not set");
 
                 // This is used to expand the definitions in the path aliases file.
                 subs.insert("$HOME", &home_dir);
@@ -65,10 +89,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let tilde = "~".to_string();
                 subs.insert(&home_dir, &tilde);
 
-                let request_url = format!(
-                    "http://{}:{}/path-shorten",
-                    settings.server.domain, settings.server.port
-                );
+                let request_url = format!("{}/path-shorten", request_base_url);
 
                 let json_body = json!({
                     "name": path_to_shorten,
@@ -83,21 +104,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 print!("{}", path_shortened.path_shortened);
             }
         }
+        Some("paths-sort") => {
+            if let Some(m) = matches.subcommand_matches("paths-sort") {
+                let paths_to_sort = m.value_of("paths_to_sort").unwrap();
+                let priorities_raw_file = m.value_of("priorities_raw_file").unwrap();
+                let priorities_raw_contents =
+                    fs::read_to_string(priorities_raw_file).unwrap_or_default();
+
+                let request_url = format!("{}/paths-sort", request_base_url);
+
+                let json_body = json!({
+                    "paths": paths_to_sort,
+                    "priorities_raw": priorities_raw_contents,
+                    "home": home_dir,
+                });
+
+                let response = client.post(request_url).json(&json_body).send()?;
+
+                let paths_sorted: PathsSorted = response.json()?;
+
+                print!("{}", paths_sorted.paths_sorted);
+            }
+        }
         Some("ping") => {
-            let request_url = format!(
-                "http://{}:{}/ping",
-                settings.server.domain, settings.server.port
-            );
+            let request_url = format!("{}/ping", request_base_url);
 
             client.get(request_url).send()?;
 
             println!("OK");
         }
         Some("shutdown") => {
-            let request_url = format!(
-                "http://{}:{}/shutdown",
-                settings.server.domain, settings.server.port
-            );
+            let request_url = format!("{}/shutdown", request_base_url);
 
             let response = client.get(request_url).send()?;
 
