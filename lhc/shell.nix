d43aa546596c474351ddf9889f20a227a1b5b1cc
Linus Arver <linusarver@gmail.com>
2022-04-12 10:07:52 -0700

    lhc: clean up nix-shell

--- lhc/shell.nix
+++ lhc/shell.nix
@@ -1,9 +1,14 @@
 { profile ? "default" }:
 with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 let
+  # See available packages for darwin.apple_sdk.frameworks with
+  #
+  #  $ nix-env -f '<nixpkgs>' -qaPA darwin.apple_sdk.frameworks
   platform-specific = if stdenv.hostPlatform.isDarwin then
-    darwin.apple_sdk.frameworks.Security else "";
+    (with darwin.apple_sdk.frameworks; [
+      Security
+    ]) else [];
 in mkShell {
   nativeBuildInputs =
-    [ rust-bin.stable.latest.${profile} pkg-config openssl platform-specific ];
+    [ rust-bin.stable.latest.${profile} pkg-config openssl ] ++ platform-specific;
 }
