00b62a19d4f27a0887da953b75d1cd26ffecb15a
Linus Arver <linusarver@gmail.com>
2022-08-06 21:43:59 +1000

    refactor

--- script/tmux_move_pane.sh
+++ script/tmux_move_pane.sh
@@ -7,35 +7,41 @@
 
 set -euo pipefail
 
-__session_id="${1}"
-__window_index="${2}"
-__direction="${3}"
-
 main()
 {
+	local session_name
+	local window_index
+	local direction
 
+	local target_window_index
 	local window_index_last
+
+	session_name="${1}"
+	window_index="${2}"
+	direction="${3}"
+
 	# This only works because we turn on "renumber-windows" in tmux.
 	window_index_last="$(tmux list-windows -F \#I | tail -n1)"
 
-	if [[ "${__direction}" == LEFT ]]; then
+	if [[ "${direction}" == LEFT ]]; then
 		# If we're already at the leftmost window, move the pane to the last
 		# window.
-		if (( "${__window_index}" == 0 )); then
-			tmux join-pane -t "${__session_id}:${window_index_last}"
+		if (( "${window_index}" == 0 )); then
+			target_window_index="${window_index_last}"
 		else
-			tmux join-pane -t "${__session_id}:$((__window_index - 1))"
+			target_window_index="$((window_index - 1))"
 		fi
 	else
 		# If we're at the rightmost window, then we have to move the pane to the
 		# first window.
-		if (( "${__window_index}" == "${window_index_last}" )); then
-			tmux join-pane -t "${__session_id}:0"
+		if (( "${window_index}" == "${window_index_last}" )); then
+			target_window_index="0"
 		else
-			tmux join-pane -t "${__session_id}:$((__window_index + 1))"
+			target_window_index="$((window_index + 1))"
 		fi
 	fi
 
+	tmux join-pane -t "${session_name}:${target_window_index}"
 	tmux set key-table root
 }
 
