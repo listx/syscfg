cff47e0d252ebcbfbbe7e103b624f75801650962
Linus Arver <linusarver@gmail.com>
2022-08-08 22:22:38 -0700

    lh: systemctl invocation: do not hardcode zsh path
    This requires us to install zsh via "nix-env -iA nixpkgs.zsh". This path
    is not available on k0 where we had a blank "nix-env -q" and all
    packages were available instead under /run/current-system/sw/bin/zsh and
    /usr/bin/zsh was not available.

--- lh/systemd_install.sh
+++ lh/systemd_install.sh
@@ -2,5 +2,9 @@
 
 set -euo pipefail
 
+mkdir -p ~/.config/systemd/user
+pushd ~/.config/systemd/user
+ln -sf ~/syscfg/lh/lh.service
+
 systemctl --user daemon-reload
 systemctl --user start lh.service
