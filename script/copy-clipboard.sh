69134960c53e0f435c44eceb6832332eb026cdbe
Linus Arver <linusarver@gmail.com>
2022-08-11 00:29:15 -0700

    copy-clipboard: prefer to copy into tmux buffer
    Now we can press "y" in tig on a commit, even from within a nested tmux
    session over ssh, and have it copy the SHA into our local clipboard.

--- script/copy-clipboard.sh
+++ script/copy-clipboard.sh
@@ -12,7 +12,13 @@ copy_clipboard()
 
 main()
 {
-    copy_clipboard
+	# Prefer to copy into tmux if possible, because it can also (with the -w
+	# flag) attempt to copy into the system clipboard.
+	if [[ -n "${TMUX:-}" ]]; then
+		tmux load-buffer -w -
+	else
+		copy_clipboard
+	fi
 }
 
 main
