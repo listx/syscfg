30d09be6f4a9c9184a9d5bd5687f527f44312ca7
Linus Arver <linusarver@gmail.com>
2023-04-20 22:19:59 -0700

    emacs: add citeproc

--- emacs/doom-cfg/packages.el
+++ emacs/doom-cfg/packages.el
@@ -47,6 +47,7 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+(package! citeproc)
 (package! git-gutter)
 (package! protobuf-mode)
 (package! solaire-mode :disable t)
