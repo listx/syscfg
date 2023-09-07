cb76dd4ecc498aa090b162e11f45a6dac4caf463
Linus Arver <linus@ucla.edu>
2023-09-07 09:11:00 -0700

    nixos: update binary cache
    See https://github.com/input-output-hk/ops-lib/commit/2fdfc6051e69f66d9a8fce7f9a09cf6bc8dd185e.

--- nixos/k0/configuration.nix
+++ nixos/k0/configuration.nix
@@ -90,5 +90,5 @@
   # Binary Cache for Haskell.nix
   nix.settings.trusted-public-keys =
     [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
-  nix.settings.substituters = [ "https://hydra.iohk.io" ];
+  nix.settings.substituters = [ "https://cache.iog.io" ];
 }
