27b8342fad52a0e65e828574b87c4e7698b63580
Linus Arver <linus@ucla.edu>
2024-04-15 20:31:57 -0700

    darwin: disable karabiner-elements
    It doesn't seem to work any more. Disable for now (use Karabiner
    manually).

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -56,9 +56,6 @@ in
       zstd
     ];
 
-  # Enable Karabiner-Elements.
-  services.karabiner-elements.enable = true;
-
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
