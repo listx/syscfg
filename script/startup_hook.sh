d17c0c46ecb39ee279e64925e686535b55a617c5
Linus Arver <linus@ucla.edu>
2024-08-10 10:10:36 +1000

    startup_hook: add docs

--- script/startup_hook.sh
+++ script/startup_hook.sh
@@ -4,7 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Start up various background services.
+# Start up various background services. This script is expected to be called
+# from XMonad on startup. The advantage of using this script is that we don't
+# have to recompile XMonad every time we want to change something here (as this
+# script is just Bash, not Haskell).
 
 case $(hostname) in
 k0)
