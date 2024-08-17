d6d035075fae631205facac85bbbadedba33f5d5
Linus Arver <linus@ucla.edu>
2024-08-18 08:48:58 +1000

    wezterm: formatting
    In e6930c43 (org-mode: delete trailing whitespace after tangling,
    2024-08-04) we cleaned up trailing whitespace after tangling. Since then
    we've never tangled the WezTerm configs, until now.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -62,7 +62,7 @@ local general_config = {
     foreground = '#e5e7ea',
     -- The default background color
     background = '#343c48',
-  
+
     -- Overrides the cell background color when the current cell is occupied by the
     -- cursor and the cursor style is set to Block
     cursor_bg = '#ffffff',
@@ -72,12 +72,12 @@ local general_config = {
     -- or the color of the vertical or horizontal bar when the cursor style is set to
     -- Bar or Underline.
     cursor_border = '#52ad70',
-  
+
     -- the foreground color of selected text
     selection_fg = '#000000',
     -- the background color of selected text
     selection_bg = '#fffacd',
-  
+
     ansi = {
       '#22222f',
       '#e49f9f',
@@ -98,13 +98,13 @@ local general_config = {
       '#b4f0f0',
       '#ffffff',
     },
-  
+
     -- Since: 20220319-142410-0fcdea07
     -- When the IME, a dead key or a leader key are being processed and are effectively
     -- holding input pending the result of input composition, change the cursor
     -- to this color to give a visual cue about the compose state.
     compose_cursor = 'orange',
-  
+
     -- Colors for copy_mode and quick_select
     -- available since: 20220807-113146-c2fee766
     -- In copy_mode, the color of the active text is:
@@ -118,7 +118,7 @@ local general_config = {
     copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
     copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
     copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
-  
+
     quick_select_label_bg = { Color = 'peru' },
     quick_select_label_fg = { Color = '#ffffff' },
     quick_select_match_bg = { AnsiColor = 'Navy' },
@@ -358,9 +358,9 @@ local general_config = {
     -- C-@ (that is, C-S-2) by default sends a literal NUL character. This is pretty
     -- much useless so we create a separate mapping here.
     bind("@", CS, "\x1b[24~\x1b[64;5u"), -- C-@ (C-S-2)
-    
+
     -- C-A to C-Z (codepoints 65-90) are handled already above.
-    
+
     -- C-\ by default sends 0x1c, which is the FS (file separator) key. See
     -- https://en.wikipedia.org/wiki/Control-%5C. Most UNIX programs interpret this
     -- as a SIGQUIT, but in Emacs it's the default keystroke mapping for
@@ -378,7 +378,7 @@ local general_config = {
     bind("|", CS, "\x1b[92;6u"), -- C-| (C-S-\)
     bind("}", CS, "\x1b[93;6u"), -- C-} (C-S-])
     bind("~", CS, "\x1b[96;6u"), -- C-~ (C-S-`)
-    
+
     bind("[", M,   "\x1b[24~\x1b[91;3u"), -- M-[
     bind("[", MS,  "\x1b[24~\x1b[91;4u"), -- M-S-[ (M-{)
     bind("[", C,   "\x1b[24~\x1b[91;5u"), -- C-[
