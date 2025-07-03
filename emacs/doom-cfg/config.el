930feb4b7ee67fee91239a9e4cf27430f6373fde
Linus Arver <linus@ucla.edu>
2025-07-03 20:58:48 +1000

    emacs org: update custom agenda view to Org 9.7
    The variable "PRIORITY" has been renamed to "URGENCY" [1] [2], so rename
    our settings to reflect this to keep the old behavior.
    
    [1] https://www.reddit.com/r/orgmode/comments/1iy6hrf/update_to_emacs_301orgmode_97_changed_agenda_sort/
    [2] https://orgmode.org/Changes.html#:~:text=%22Priority%22%20used%20to%20sort%20items%20in%20agenda%20is%20renamed%20to%20%22urgency%22

--- emacs/doom-cfg/config.el
+++ emacs/doom-cfg/config.el
@@ -742,7 +742,7 @@ LINK-NAME."
          ;; scheduled date is today or in the past. This way we only concern
          ;; ourselves with tasks that we can actually work on.
          ((tags
-           "PRIORITY>=\"0\""
+           "URGENCY>=\"0\""
            ((org-agenda-skip-function
              '(or
                ;; Skip entries if they haven't been scheduled yet.
